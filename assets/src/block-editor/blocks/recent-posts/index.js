7664d0be26e9cacf464a927a9dc2ed670e9ea482
David Lonjon <david.lonjon@gmail.com>
2020-02-18 20:36:40 +0900

    Update the Recent Posts block icon

--- assets/src/block-editor/blocks/recent-posts/index.js
+++ assets/src/block-editor/blocks/recent-posts/index.js
@@ -8,6 +8,17 @@ import { __ } from '@wordpress/i18n';
 
 export const name = 'material/recent-posts';
 
+const el = wp.element.createElement;
+// This could allow us complete custom block icon
+const iconEl = el(
+	'svg',
+	{ width: 24, height: 24 },
+	el( 'path', {
+		d:
+			'M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z',
+	} )
+);
+
 export const settings = {
 	title: __( 'Recent Posts', 'material-theme-builder' ),
 	description: __(
@@ -15,7 +26,7 @@ export const settings = {
 		'material-theme-builder'
 	), // @todo
 	category: 'material',
-	icon: <i className="material-icons md-list">list</i>,
+	icon: iconEl,
 	edit,
 	save,
 };
