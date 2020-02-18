2c8e7a522da26bee4cbcf0e3a35f3d52ac7b5262
Dukagjin Surdulli <me@dukagj.in>
2020-02-18 17:04:47 +0100

    Apply appropriate styling to the picker and add the tooltip

--- assets/src/block-editor/blocks/button/components/icon-picker/index.js
+++ assets/src/block-editor/blocks/button/components/icon-picker/index.js
@@ -1,37 +1,58 @@
-import { useState } from 'react';
-import { TextControl } from '@wordpress/components';
+/**
+ * WordPress dependencies
+ */
+import { useState, useCallback } from 'react';
+import { __ } from '@wordpress/i18n';
+import { TextControl, Tooltip } from '@wordpress/components';
 
+/**
+ * Internal dependencies
+ */
 import icons from './mdi.json';
 import './icon-picker.css';
 
 export default ( { currentIcon, pickHandler } ) => {
 	const [ filteredIcons, setFilteredIcons ] = useState( icons );
 
-	const filterIcons = filterText => {
-		setFilteredIcons(
-			icons.filter( icon => icon.class.includes( filterText ) )
-		);
-	};
+	const filterIcons = useCallback(
+		filterText => {
+			setFilteredIcons(
+				icons.filter( icon => icon.class.includes( filterText ) )
+			);
+		},
+		[ setFilteredIcons, icons ]
+	);
 
 	const iconsRender = filteredIcons.map( icon => {
+		const isSelected =
+			currentIcon === icon.class
+				? ' icons-container__icon__icon-btn--active'
+				: '';
+
 		return (
-			<button
-				key={ icon.class }
-				type="button"
-				className={ `icons-container__icon ${
-					currentIcon === icon.class ? 'icons-container__icon--active' : ''
-				}` }
-				onClick={ pickHandler.bind( this, icon.class ) }
-			>
-				<i className={ `material-icons md-${ icon.class }` }>{ icon.label }</i>
-			</button>
+			<div key={ icon.class } className="icons-container__icon">
+				<Tooltip text={ icon.label }>
+					<button
+						type="button"
+						className={ `icons-container__icon__icon-btn${ isSelected }` }
+						onClick={ pickHandler.bind( this, icon.class ) }
+					>
+						<i className={ `material-icons md-${ icon.class }` }>
+							{ icon.label }
+						</i>
+					</button>
+				</Tooltip>
+			</div>
 		);
 	} );
 
 	return (
 		<>
 			<section>
-				<TextControl label="Search" onChange={ filterIcons } />
+				<TextControl
+					label={ __( 'Search icon', 'material-theme-builder' ) }
+					onChange={ filterIcons }
+				/>
 			</section>
 			<section className="icons-container">{ iconsRender }</section>
 		</>
