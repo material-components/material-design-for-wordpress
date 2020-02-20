8044b40132e27baa09b75af85bf25b874d5cd368
David Lonjon <david.lonjon@gmail.com>
2020-02-20 15:07:39 +0900

    Refactor from class component to functional component

--- assets/src/block-editor/blocks/recent-posts/components/inspector-controls.js
+++ assets/src/block-editor/blocks/recent-posts/components/inspector-controls.js
@@ -3,7 +3,8 @@
  */
 import { __ } from '@wordpress/i18n';
 import { InspectorControls } from '@wordpress/block-editor';
-import { Component } from '@wordpress/element';
+import { useState, useEffect } from '@wordpress/element';
+
 import {
 	PanelBody,
 	RangeControl,
@@ -35,146 +36,136 @@ const MAX_POSTS_COLUMNS = 10;
 
 /**
  * Recent Posts Inspector Controls
+ *
+ * @param props
  */
-export default class RecentPostsInspectorControls extends Component {
-	constructor() {
-		super( ...arguments );
-		this.state = {
-			categoriesList: [],
-		};
-	}
+export default props => {
+	const [ categoriesList, setCategoriesList ] = useState( [] );
+	const [ isStillMounted, setStillMounted ] = useState( true );
 
-	componentDidMount() {
-		this.isStillMounted = true;
-		this.fetchRequest = apiFetch( {
+	useEffect( () => {
+		setStillMounted( true );
+		apiFetch( {
 			path: addQueryArgs( `/wp/v2/categories`, CATEGORIES_LIST_QUERY ),
 		} )
-			.then( categoriesList => {
-				if ( this.isStillMounted ) {
-					this.setState( { categoriesList } );
+			.then( fetchedCategoriesList => {
+				if ( isStillMounted ) {
+					setCategoriesList( fetchedCategoriesList );
 				}
 			} )
 			.catch( () => {
-				if ( this.isStillMounted ) {
-					this.setState( { categoriesList: [] } );
+				if ( isStillMounted ) {
+					setCategoriesList( [] );
 				}
 			} );
-	}
+	}, [] );
 
-	componentWillUnmount() {
-		this.isStillMounted = false;
-	}
+	useEffect( () => {
+		return () => {
+			setStillMounted( false );
+		};
+	}, [] );
 
-	render() {
-		const { attributes, setAttributes } = this.props;
+	const { attributes, setAttributes } = props;
 
-		const {
-			style,
-			columns,
-			postsToShow,
-			outlined,
-			displayPostDate,
-			displayPostContent,
-			displayFeaturedImage,
-			displayCommentsCount,
-			displayPostAuthor,
-			categories,
-		} = attributes;
+	const {
+		style,
+		columns,
+		postsToShow,
+		outlined,
+		displayPostDate,
+		displayPostContent,
+		displayFeaturedImage,
+		displayCommentsCount,
+		displayPostAuthor,
+		categories,
+	} = attributes;
 
-		const { categoriesList } = this.state;
+	const styleOptions = [
+		{
+			label: __( 'Stacked', 'material-theme-builder' ),
+			value: 'stacked',
+			src: StackedStyleIcon,
+		},
+		{
+			label: __( 'Horizontal', 'material-theme-builder' ),
+			value: 'horizontal',
+			src: HorizontalStyleIcon,
+		},
+	];
 
-		const styleOptions = [
-			{
-				label: __( 'Stacked', 'material-theme-builder' ),
-				value: 'stacked',
-				src: StackedStyleIcon,
-			},
-			{
-				label: __( 'Horizontal', 'material-theme-builder' ),
-				value: 'horizontal',
-				src: HorizontalStyleIcon,
-			},
-		];
-
-		return (
-			<InspectorControls>
-				<PanelBody title={ __( 'Style', 'material-theme-builder' ) }>
-					<ImageRadioControl
-						selected={ style }
-						options={ styleOptions }
-						onChange={ value => setAttributes( { style: value } ) }
-					/>
-				</PanelBody>
-				<PanelBody title={ __( 'Content', 'material-theme-builder' ) }>
-					{ style === 'stacked' && (
-						<RangeControl
-							label={ __( 'Columns', 'material-theme-builder' ) }
-							value={ columns }
-							onChange={ value => setAttributes( { columns: value } ) }
-							min={ 2 }
-							max={ MAX_POSTS_COLUMNS }
-						/>
-					) }
+	return (
+		<InspectorControls>
+			<PanelBody title={ __( 'Style', 'material-theme-builder' ) }>
+				<ImageRadioControl
+					selected={ style }
+					options={ styleOptions }
+					onChange={ value => setAttributes( { style: value } ) }
+				/>
+			</PanelBody>
+			<PanelBody title={ __( 'Content', 'material-theme-builder' ) }>
+				{ style === 'stacked' && (
 					<RangeControl
-						label={ __( 'Number of posts', 'material-theme-builder' ) }
-						value={ postsToShow }
-						onChange={ value => setAttributes( { postsToShow: value } ) }
-						min={ 1 }
-						max={ MAX_NUMBER_OF_POSTS }
+						label={ __( 'Columns', 'material-theme-builder' ) }
+						value={ columns }
+						onChange={ value => setAttributes( { columns: value } ) }
+						min={ 2 }
+						max={ MAX_POSTS_COLUMNS }
 					/>
-					<ToggleControl
-						label={ __( 'Outlined', 'material-theme-builder' ) }
-						checked={ outlined }
-						onChange={ value => setAttributes( { outlined: value } ) }
-					/>
-				</PanelBody>
+				) }
+				<RangeControl
+					label={ __( 'Number of posts', 'material-theme-builder' ) }
+					value={ postsToShow }
+					onChange={ value => setAttributes( { postsToShow: value } ) }
+					min={ 1 }
+					max={ MAX_NUMBER_OF_POSTS }
+				/>
+				<ToggleControl
+					label={ __( 'Outlined', 'material-theme-builder' ) }
+					checked={ outlined }
+					onChange={ value => setAttributes( { outlined: value } ) }
+				/>
+			</PanelBody>
 
-				<PanelBody
-					title={ __( 'Post meta settings', 'material-theme-builder' ) }
-				>
-					<ToggleControl
-						label={ __( 'Post date', 'material-theme-builder' ) }
-						checked={ displayPostDate }
-						onChange={ value => setAttributes( { displayPostDate: value } ) }
-					/>
-					<ToggleControl
-						label={ __( 'Post content', 'material-theme-builder' ) }
-						checked={ displayPostContent }
-						onChange={ value => setAttributes( { displayPostContent: value } ) }
-					/>
-					<ToggleControl
-						label={ __( 'Featured Image', 'material-theme-builder' ) }
-						checked={ displayFeaturedImage }
-						onChange={ value =>
-							setAttributes( { displayFeaturedImage: value } )
-						}
-					/>
-					<ToggleControl
-						label={ __( 'Comments Count', 'material-theme-builder' ) }
-						checked={ displayCommentsCount }
-						onChange={ value =>
-							setAttributes( { displayCommentsCount: value } )
-						}
-					/>
-					<ToggleControl
-						label={ __( 'Post Author', 'material-theme-builder' ) }
-						checked={ displayPostAuthor }
-						onChange={ value => setAttributes( { displayPostAuthor: value } ) }
-					/>
-				</PanelBody>
+			<PanelBody title={ __( 'Post meta settings', 'material-theme-builder' ) }>
+				<ToggleControl
+					label={ __( 'Post date', 'material-theme-builder' ) }
+					checked={ displayPostDate }
+					onChange={ value => setAttributes( { displayPostDate: value } ) }
+				/>
+				<ToggleControl
+					label={ __( 'Post content', 'material-theme-builder' ) }
+					checked={ displayPostContent }
+					onChange={ value => setAttributes( { displayPostContent: value } ) }
+				/>
+				<ToggleControl
+					label={ __( 'Featured Image', 'material-theme-builder' ) }
+					checked={ displayFeaturedImage }
+					onChange={ value => setAttributes( { displayFeaturedImage: value } ) }
+				/>
+				<ToggleControl
+					label={ __( 'Comments Count', 'material-theme-builder' ) }
+					checked={ displayCommentsCount }
+					onChange={ value => setAttributes( { displayCommentsCount: value } ) }
+				/>
+				<ToggleControl
+					label={ __( 'Post Author', 'material-theme-builder' ) }
+					checked={ displayPostAuthor }
+					onChange={ value => setAttributes( { displayPostAuthor: value } ) }
+				/>
+			</PanelBody>
 
-				<PanelBody title={ __( 'Filtering', 'material-theme-builder' ) }>
-					<QueryControls
-						categoriesList={ categoriesList }
-						selectedCategoryId={ categories }
-						onCategoryChange={ value =>
-							setAttributes( {
-								categories: '' !== value ? value : undefined,
-							} )
-						}
-					/>
-				</PanelBody>
-			</InspectorControls>
-		);
-	}
-}
+			<PanelBody title={ __( 'Filtering', 'material-theme-builder' ) }>
+				<QueryControls
+					categoriesList={ categoriesList }
+					selectedCategoryId={ categories }
+					onCategoryChange={ value =>
+						setAttributes( {
+							categories: '' !== value ? value : undefined,
+						} )
+					}
+				/>
+			</PanelBody>
+		</InspectorControls>
+	);
+};
