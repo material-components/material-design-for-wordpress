b5c4ca79066be5fddd0d52a94774f281c172b277
David Lonjon <david.lonjon@gmail.com>
2020-02-20 10:57:40 +0900

    Refactor the loading of the Recent Posts block

--- assets/src/block-editor/index.js
+++ assets/src/block-editor/index.js
@@ -7,5 +7,7 @@ import { registerBlockType } from '@wordpress/blocks';
  * Internal dependencies
  */
 import * as button from './blocks/button';
+import * as recentPost from './blocks/recent-posts';
 
 registerBlockType( `material/${ button.name }`, button.settings );
+registerBlockType( `material/${ recentPost.name }`, recentPost.settings );
