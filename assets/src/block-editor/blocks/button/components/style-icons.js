8e22c04d015f5c492be0a38d52a6f1a7e6c8e07a
Dukagjin Surdulli <me@dukagj.in>
2020-02-20 14:11:52 +0100

    Fully implement button style choice

---
+++ assets/src/block-editor/blocks/button/components/style-icons.js
@@ -0,0 +1,82 @@
+export const TextIcon = () => (
+	<svg
+		width="39"
+		height="4"
+		viewBox="0 0 39 4"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<rect
+			x="0.538452"
+			y="0.290283"
+			width="37.7955"
+			height="2.77342"
+			fill="#565D66"
+		/>
+	</svg>
+);
+
+export const OutlinedIcon = () => (
+	<svg
+		width="84"
+		height="32"
+		viewBox="0 0 84 32"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<rect
+			x="1.15723"
+			y="1.48389"
+			width="81.5195"
+			height="29.1112"
+			rx="2.5"
+			stroke="#565D66"
+		/>
+		<rect
+			x="23.0195"
+			y="14.6519"
+			width="37.7955"
+			height="2.77342"
+			fill="#565D66"
+		/>
+	</svg>
+);
+
+export const RaisedIcon = () => (
+	<svg
+		width="85"
+		height="35"
+		viewBox="0 0 85 35"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<rect y="0.470703" width="85" height="34" rx="3" fill="#D9D9D9" />
+		<rect x="1" y="1.4707" width="83" height="30" rx="3" fill="#565D66" />
+		<rect
+			x="23.6023"
+			y="15.1387"
+			width="37.7955"
+			height="2.77342"
+			fill="white"
+		/>
+	</svg>
+);
+
+export const UnelevatedIcon = () => (
+	<svg
+		width="83"
+		height="31"
+		viewBox="0 0 83 31"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<rect y="0.448547" width="83" height="30" rx="3" fill="#565D66" />
+		<rect
+			x="22.6023"
+			y="14.1165"
+			width="37.7955"
+			height="2.77342"
+			fill="white"
+		/>
+	</svg>
+);
