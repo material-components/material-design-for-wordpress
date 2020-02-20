a7c189094f4aa18d5480779b4b0a223b35a08f07
Dukagjin Surdulli <me@dukagj.in>
2020-02-20 14:33:15 +0100

    Display selected icon preview beside the filter textfield

--- assets/src/block-editor/components/icon-picker/index.js
+++ assets/src/block-editor/components/icon-picker/index.js
@@ -35,8 +35,10 @@ export default ( { currentIcon, pickHandler } ) => {
 
 	const iconsRender = filteredIcons.map( icon => {
 		const iconName = toSlug( rawIcons[ icon ].name );
+		const iconHex = parseInt( icon, 16 );
+
 		const isSelected =
-			currentIcon === iconName
+			currentIcon?.name === iconName
 				? ' icons-container__icon__icon-btn--active'
 				: '';
 
@@ -46,11 +48,12 @@ export default ( { currentIcon, pickHandler } ) => {
 					<button
 						type="button"
 						className={ `icons-container__icon__icon-btn${ isSelected }` }
-						onClick={ pickHandler.bind( this, iconName ) }
+						onClick={ pickHandler.bind( this, {
+							name: iconName,
+							hex: iconHex,
+						} ) }
 					>
-						<i className="material-icons">
-							{ String.fromCharCode( parseInt( icon, 16 ) ) }
-						</i>
+						<i className="material-icons">{ String.fromCharCode( iconHex ) }</i>
 					</button>
 				</Tooltip>
 			</div>
@@ -59,11 +62,20 @@ export default ( { currentIcon, pickHandler } ) => {
 
 	return (
 		<>
-			<section>
-				<TextControl
-					label={ __( 'Search icon', 'material-theme-builder' ) }
-					onChange={ filterIcons }
-				/>
+			<section className="icons-search">
+				<div className="icons-search__search-input">
+					<TextControl
+						label={ __( 'Search icon', 'material-theme-builder' ) }
+						onChange={ filterIcons }
+					/>
+				</div>
+				{ currentIcon && (
+					<div className="icons-search__selected-icon">
+						<i className="material-icons">
+							{ String.fromCharCode( currentIcon?.hex ) }
+						</i>
+					</div>
+				) }
 			</section>
 
 			<section className="icons-container">{ iconsRender }</section>
