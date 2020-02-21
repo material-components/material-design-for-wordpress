f4370dee858d74ebaf3d05aba3545dd40a595293
Dukagjin Surdulli <me@dukagj.in>
2020-02-21 21:36:49 +0100

    Set up block unit tests

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
 	preset: '@wordpress/jest-preset-default',
 	collectCoverageFrom: [ 'assets/src/**/*.js' ],
 	testPathIgnorePatterns: [ '/node_modules/', '/vendor/', '/bin/' ],
+	moduleNameMapper: {
+		'^!!json-loader!(.*).ijmap$': '<rootDir>/tests/js/utils/ijmap-mock.js',
+	},
 };
