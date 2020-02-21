ec463b30eca7b0a04ed5e0b80b8a7d5a5bc24536
David Lonjon <david.lonjon@gmail.com>
2020-02-21 21:32:14 +0900

    Break each child single post card components into their own file

---
+++ assets/src/block-editor/blocks/recent-posts/components/card-image.js
@@ -0,0 +1,8 @@
+const CardImage = ( { imageSourceUrl, type } ) => (
+	<div
+		className={ `mdc-card__media mdc-card__media--${ type } single-post-card__media }` }
+		style={ { backgroundImage: `url(${ imageSourceUrl })` } }
+	/>
+);
+
+export default CardImage;
---
+++ assets/src/block-editor/blocks/recent-posts/components/vertical-card-layout.js
@@ -0,0 +1,65 @@
+/**
+ * WordPress dependencies
+ */
+import { RawHTML } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import CardImage from './card-image';
+import CardPrimary from './card-primary';
+import CardActions from './card-actions';
+
+const VerticalCardLayout = props => {
+	const {
+		postIndex,
+		excerpt,
+		imageSourceUrl,
+		outlined,
+		displayPostContent,
+		postContentLength,
+		displayFeaturedImage,
+		displayCommentsCount,
+		displayPostAuthor,
+	} = props;
+
+	return (
+		<div
+			className={
+				'mdc-card ' +
+				( outlined ? 'mdc-card--outlined' : '' ) +
+				' single-post-card single-post-card__masonry single-post-basic'
+			}
+		>
+			<div
+				className="mdc-card__primary-action single-post-card__primary-action mdc-ripple-upgraded"
+				tabIndex={ postIndex }
+			>
+				{ displayFeaturedImage && imageSourceUrl && (
+					<CardImage imageSourceUrl={ imageSourceUrl } type="16-9" />
+				) }
+				<CardPrimary { ...props } />
+				{ displayPostContent && (
+					<div className="single-post-card__secondary mdc-typography mdc-typography--body2">
+						<RawHTML key="html">
+							{ postContentLength < excerpt.trim().split( ' ' ).length
+								? excerpt
+										.trim()
+										.split( ' ', postContentLength )
+										.join( ' ' ) + ' ...'
+								: excerpt
+										.trim()
+										.split( ' ', postContentLength )
+										.join( ' ' ) }
+						</RawHTML>
+					</div>
+				) }
+			</div>
+			{ ( displayPostAuthor || displayCommentsCount ) && (
+				<CardActions { ...props } />
+			) }
+		</div>
+	);
+};
+
+export default VerticalCardLayout;
