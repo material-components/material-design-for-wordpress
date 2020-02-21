22b4247437e11af2fe983113eb741c0465273e77
David Lonjon <david.lonjon@gmail.com>
2020-02-21 16:08:55 +0900

    Add missing translation text domain

--- assets/src/block-editor/blocks/recent-posts/components/inspector-controls.js
+++ assets/src/block-editor/blocks/recent-posts/components/inspector-controls.js
@@ -143,7 +143,7 @@ const RecentPostsInspectorControls = props => {
 				/>
 				{ displayPostContent && (
 					<RangeControl
-						label={ __( 'Max number of words in post content' ) }
+						label={ __( 'Max number of words in post content', 'material-theme-builder' ) }
 						value={ postContentLength }
 						onChange={ value => setAttributes( { postContentLength: value } ) }
 						min={ MIN_POST_CONTENT_LENGTH }
