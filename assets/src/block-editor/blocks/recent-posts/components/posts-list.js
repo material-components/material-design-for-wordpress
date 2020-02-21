891bccc4c33eed08f66ca4b658fedbca7417a88b
David Lonjon <david.lonjon@gmail.com>
2020-02-21 21:07:49 +0900

    Add comment

--- assets/src/block-editor/blocks/recent-posts/components/posts-list.js
+++ assets/src/block-editor/blocks/recent-posts/components/posts-list.js
@@ -15,6 +15,11 @@ const PostsList = ( { attributes, setAttributes, recentPosts } ) => {
 	let columnSpan = 12;
 
 	if ( style === 'grid' ) {
+		/*
+		 * This works well for the design if we have a maximum of 4 columns. It would not work
+		 * so well for 5 and 7 columns for example. Something to keep in mind if the max number of columns
+		 * increase above 4.
+		 */
 		columnSpan = Math.floor( 12 / columns );
 	}
 
