9e451b5896075af5035256556a0cf9636878d863
Ravi Chandra <ravichandrach1@gmail.com>
2020-02-23 19:35:45 +0530

    Add templates and partials for recent posts block

--- assets/src/block-editor/blocks/recent-posts/components/card-image.js
+++ assets/src/block-editor/blocks/recent-posts/components/card-image.js
@@ -1,6 +1,6 @@
 const CardImage = ( { imageSourceUrl, type } ) => (
 	<div
-		className={ `mdc-card__media mdc-card__media--${ type } single-post-card__media }` }
+		className={ `mdc-card__media mdc-card__media--${ type } single-post-card__media` }
 		style={ { backgroundImage: `url(${ imageSourceUrl })` } }
 	/>
 );
