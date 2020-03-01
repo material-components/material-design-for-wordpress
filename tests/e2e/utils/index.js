3bbfa23e3d61f7dcaaacb0d331b3d2ae137fc2a5
Derek Herman <derek.herman@xwp.co>
2020-02-29 20:46:34 -0800

    Setup e2e tests

---
+++ tests/e2e/utils/index.js
@@ -0,0 +1,5 @@
+export { activatePlugin } from './activate-plugin';
+export { deactivatePlugin } from './deactivate-plugin';
+export { clickButton } from './click-button';
+export { clickButtonByLabel } from './click-button-by-label';
+export { insertBlock } from './insert-block';
