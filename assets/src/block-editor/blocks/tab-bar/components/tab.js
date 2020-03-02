4374bd3dbe89f1c4ea88cd5e36426d079930bb74
Dukagjin Surdulli <me@dukagj.in>
2020-03-02 20:24:15 +0100

    Refactor and optimize the code
    - Remove all "active" keys in association with a tab
    - Use index to determine active tab and other tab operations
    - Make use of useCallback for optimization

--- assets/src/block-editor/blocks/tab-bar/components/tab.js
+++ assets/src/block-editor/blocks/tab-bar/components/tab.js
@@ -3,55 +3,50 @@
  */
 import classNames from 'classnames';
 
-const Tab = ( { icon, label, active, onChange, onDelete, iconPosition } ) => {
-	return (
-		<div
-			role="tab"
-			tabIndex={ 0 }
-			onKeyDown={ () => {} }
-			onClick={ onChange }
-			className={ classNames( 'mdc-tab', 'tab', {
-				'mdc-tab--active': active,
-				'mdc-tab--stacked': icon && iconPosition === 'above',
-			} ) }
-		>
-			<span className="mdc-tab__content">
-				{ icon && iconPosition !== 'none' && (
-					<i className="material-icons mdc-tab__icon">
-						{ String.fromCharCode( icon?.hex ) }
-					</i>
-				) }
-				<span className="mdc-tab__text-label tab__label-field">
-					<span
-						role="textbox"
-						tabIndex={ 0 }
-						contentEditable
-						suppressContentEditableWarning
-						onKeyPress={ event =>
-							event.key === 'Enter' && event.currentTarget.blur()
-						}
-					>
-						{ label }
-					</span>
+const Tab = ( { icon, label, active, onChange, onDelete, iconPosition } ) => (
+	<div
+		role="tab"
+		tabIndex={ 0 }
+		onClick={ onChange }
+		className={ classNames( 'mdc-tab', 'tab', {
+			'mdc-tab--active': active,
+			'mdc-tab--stacked': icon && iconPosition === 'above',
+		} ) }
+	>
+		<span className="mdc-tab__content">
+			{ icon && iconPosition !== 'none' && (
+				<i className="material-icons mdc-tab__icon">
+					{ String.fromCharCode( icon?.hex ) }
+				</i>
+			) }
+			<span className="mdc-tab__text-label tab__label-field">
+				<span
+					role="textbox"
+					tabIndex={ 0 }
+					contentEditable
+					suppressContentEditableWarning
+					onKeyPress={ event =>
+						event.key === 'Enter' && event.currentTarget.blur()
+					}
+				>
+					{ label }
 				</span>
 			</span>
-			{ active && (
-				<span className="mdc-tab-indicator mdc-tab-indicator--active">
-					<span className="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
-				</span>
-			) }
-			<button className="material-icons tab__delete" onClick={ onDelete }>
-				cancel
-			</button>
-		</div>
-	);
-};
-
+		</span>
+		{ active && (
+			<span className="mdc-tab-indicator mdc-tab-indicator--active">
+				<span className="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+			</span>
+		) }
+		<button className="material-icons tab__delete" onClick={ onDelete }>
+			cancel
+		</button>
+	</div>
+);
 class TabSchema {
-	constructor( { label, position, icon, content = null, active = false } ) {
+	constructor( { label, position, icon, content = null } ) {
 		this.label = label;
 		this.position = position;
-		this.active = active;
 		this.icon = icon;
 		this.content = content;
 	}
