fa39382c00422e25597d8c360fb3b8d747b0014b
David Lonjon <david.lonjon@gmail.com>
2020-03-04 20:55:17 +0900

    Add a new e2e util function to select a block by name

--- tests/e2e/utils/index.js
+++ tests/e2e/utils/index.js
@@ -3,3 +3,4 @@ export { deactivatePlugin } from './deactivate-plugin';
 export { clickButton } from './click-button';
 export { clickButtonByLabel } from './click-button-by-label';
 export { insertBlock } from './insert-block';
+export { selectBlockByName } from './select-block-by-name';
