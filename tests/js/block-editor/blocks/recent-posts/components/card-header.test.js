81e3bd56f5be659edcf362f40d63c6a533462231
David Lonjon <david.lonjon@gmail.com>
2020-03-05 21:20:35 +0900

    Add unit tests for the CardHeader component

---
+++ tests/js/block-editor/blocks/recent-posts/components/card-header.test.js
@@ -0,0 +1,42 @@
+/**
+ * External dependencies
+ */
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+
+/**
+ * Internal dependencies
+ */
+import CardHeader from '../../../../../../assets/src/block-editor/blocks/recent-posts/components/card-header';
+
+/**
+ * Render the component.
+ *
+ * @param {Object} props - Component props
+ * @return {Function} A functional component.
+ */
+const setup = props => {
+	return render( <CardHeader { ...props } /> );
+};
+
+const baseProps = {
+	titleTrimmed: 'Test Title',
+	displayPostDate: true,
+	post: {
+		date_gmt: ' 2020-02-25T08:20:38',
+	},
+	dateFormat: 'F j, Y',
+};
+
+describe( 'CardActions', () => {
+	it( 'matches snapshot when the post date should be displayed', () => {
+		const wrapper = setup( baseProps );
+		expect( wrapper ).toMatchSnapshot();
+	} );
+
+	it( 'matches snapshot when the post date should not be displayed', () => {
+		baseProps.displayPostDate = false;
+		const wrapper = setup( baseProps );
+		expect( wrapper ).toMatchSnapshot();
+	} );
+} );
