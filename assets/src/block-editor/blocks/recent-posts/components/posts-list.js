ee89ba1f1c951c9237dee11f7524cec1f065f87c
David Lonjon <david.lonjon@gmail.com>
2020-03-05 23:51:52 +0900

    Fix JSDoc

--- assets/src/block-editor/blocks/recent-posts/components/posts-list.js
+++ assets/src/block-editor/blocks/recent-posts/components/posts-list.js
@@ -15,7 +15,7 @@ import SinglePost from './single-post';
  * @param {Object} props - Component props.
  * @param {Object} props.attributes - Block attributes.
  * @param {Function} props.setAttributes - Function to set block attributes value.
- * @param {Object} props.recentPosts - Posts.
+ * @param {array} props.recentPosts - Posts.
  *
  * @return {Function} A functional component.
  */
