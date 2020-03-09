b870719b4a351a8e70ca89942f896156fb5e8fff
David Lonjon <david.lonjon@gmail.com>
2020-03-09 22:08:13 +0900

    Fix code formatting and JSDoc

--- tests/js/block-editor/blocks/hand-picked-posts/components/block-controls.test.js
+++ tests/js/block-editor/blocks/hand-picked-posts/components/block-controls.test.js
@@ -3,13 +3,14 @@
  */
 import '@testing-library/jest-dom/extend-expect';
 import { shallow } from 'enzyme';
+
 /**
  * Internal dependencies
  */
 import HandPickedPostsBlockControls from '../../../../../../assets/src/block-editor/blocks/hand-picked-posts/components/block-controls';
 
 /**
- * Render the component.
+ * Shallow render the component.
  *
  * @param {Object} props - Component props
  * @return {Function} A functional component.
