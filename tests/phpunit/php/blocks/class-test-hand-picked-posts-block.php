fbb5f151cf7d6c9c5b7f70895fbe3b20eca10ff6
David Lonjon <david.lonjon@gmail.com>
2020-03-09 17:51:26 +0900

    Add unit tests for the hand picked posts block and refactor recent posts block tests to remove code duplication

--- tests/phpunit/php/blocks/class-test-hand-picked-posts-block.php
+++ tests/phpunit/php/blocks/class-test-hand-picked-posts-block.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Tests for Recent_Posts_Block class.
+ * Tests for Hand_Picked_Posts_Block class.
  *
  * @package MaterialThemeBuilder
  */
@@ -10,18 +10,18 @@ namespace MaterialThemeBuilder\Blocks;
 use MaterialThemeBuilder\Plugin;
 
 /**
- * Tests for Recent_Posts_Block class.
+ * Tests for Hand_Picked_Posts_Block class.
  */
-class Test_Recent_Posts_Block extends \WP_UnitTestCase {
+class Test_Hand_Picked_Posts_Block extends \WP_UnitTestCase {
 	use Trait_Posts_List_Tests_Helpers;
 
 	/**
 	 * Test init.
 	 *
-	 * @see Recent_Posts_Block::init()
+	 * @see Hand_Picked_Posts_Block::init()
 	 */
 	public function test_init() {
-		$block = new Recent_Posts_Block( new Plugin() );
+		$block = new Hand_Picked_Posts_Block( new Plugin() );
 		$block->init();
 		$this->assertEquals( 10, has_filter( 'rest_prepare_post', [ $block, 'add_extra_post_meta' ] ) );
 		$this->assertEquals( 10, has_action( 'init', [ $block, 'register_block' ] ) );
@@ -30,26 +30,26 @@ class Test_Recent_Posts_Block extends \WP_UnitTestCase {
 	/**
 	 * Test register_block.
 	 *
-	 * @see Recent_Posts_Block::register_block()
+	 * @see Hand_Picked_Posts_Block::register_block()
 	 */
 	public function test_register_block() {
 		// Unregister the block if it's registered already.
-		unregister_block_type( 'material/recent-posts' );
+		unregister_block_type( 'material/hand-picked-posts' );
 
-		$block = new Recent_Posts_Block( new Plugin() );
+		$block = new Hand_Picked_Posts_Block( new Plugin() );
 		$block->register_block();
 
 		// Assert the block is registered.
-		$this->assertTrue( in_array( 'material/recent-posts', get_dynamic_block_names(), true ) );
+		$this->assertTrue( in_array( 'material/hand-picked-posts', get_dynamic_block_names(), true ) );
 	}
 
 	/**
 	 * Test add_extra_post_meta.
 	 *
-	 * @see Recent_Posts_Block::add_extra_post_meta()
+	 * @see Hand_Picked_Posts_Block::add_extra_post_meta()
 	 */
 	public function test_add_extra_post_meta() {
-		$block = new Recent_Posts_Block( new Plugin() );
+		$block = new Hand_Picked_Posts_Block( new Plugin() );
 
 		$request  = new \WP_REST_Request( 'GET', '/' );
 		$response = new \WP_REST_Response();
@@ -75,20 +75,33 @@ class Test_Recent_Posts_Block extends \WP_UnitTestCase {
 	/**
 	 * Test test_render_block.
 	 *
-	 * @see Recent_Posts_Block::test_render_block()
+	 * @see Hand_Picked_Posts_Block::test_render_block()
 	 */
 	public function test_render_block() {
-		$block = new Recent_Posts_Block( new Plugin() );
+		$block = new Hand_Picked_Posts_Block( new Plugin() );
 
 		$attributes = [
-			'postsToShow' => 5,
+			'postsToShow' => 3,
 			'style'       => 'grid-new',
 		];
 
+		// Only selecting 4 posts out of the 5 which have been created in the tests.
+		$attributes['posts'] = [ self::$post_ids[0], self::$post_ids[2], self::$post_ids[3], self::$post_ids[4] ];
+
 		$content = $block->render_block( $attributes );
 
-		// Assert no content is returned if no posts found or invalid style is selected.
+		// Assert no content is returned if invalid style is selected.
 		$this->assertContains( '<!-- No posts found -->', $content );
+
+		unset( $attributes['posts'] );
+
+		$content = $block->render_block( $attributes );
+
+		// Assert no content is returned if no posts have been picked.
+		$this->assertContains( '<!-- No posts found -->', $content );
+
+		// Only selecting 4 posts out of the 5 which have been created in the tests.
+		$attributes['posts'] = [ self::$post_ids[0], self::$post_ids[2], self::$post_ids[3], self::$post_ids[4] ];
 
 		$attributes['style'] = 'grid';
 		$content             = $block->render_block( $attributes );
@@ -102,16 +115,10 @@ class Test_Recent_Posts_Block extends \WP_UnitTestCase {
 		// Assert masonry layout is loaded.
 		$this->assertContains( '<div class="masonry-grid', $content );
 
-		// Assert 5 posts are rendered.
-		$this->assertEquals( 5, substr_count( $content, '<div class="single-post-card single-post-basic' ) );
+		// Assert 3 posts are rendered.
+		$this->assertEquals( 3, substr_count( $content, '<div class="single-post-card single-post-basic' ) );
 
-		$attributes['category'] = self::$cat_id;
-		$content                = $block->render_block( $attributes );
-
-		// Assert only 2 posts in the test category are rendered.
-		$this->assertEquals( 2, substr_count( $content, '<div class="single-post-card single-post-basic' ) );
-
-		unset( $attributes['category'] );
+		$attributes['postsToShow']          = 5;
 		$attributes['displayPostAuthor']    = true;
 		$attributes['displayCommentsCount'] = true;
 		$attributes['displayPostContent']   = true;
@@ -121,10 +128,10 @@ class Test_Recent_Posts_Block extends \WP_UnitTestCase {
 		$content = $block->render_block( $attributes );
 
 		// Assert all partials are rendered.
-		$this->assertEquals( 5, substr_count( $content, 'class="mdc-button post-author"' ) );
-		$this->assertEquals( 5, substr_count( $content, 'class="mdc-button comment-count"' ) );
-		$this->assertEquals( 5, substr_count( $content, 'class="single-post-card__secondary' ) );
-		$this->assertEquals( 5, substr_count( $content, 'class="single-post-card__subtitle' ) );
+		$this->assertEquals( 4, substr_count( $content, 'class="mdc-button post-author"' ) );
+		$this->assertEquals( 4, substr_count( $content, 'class="mdc-button comment-count"' ) );
+		$this->assertEquals( 4, substr_count( $content, 'class="single-post-card__secondary' ) );
+		$this->assertEquals( 4, substr_count( $content, 'class="single-post-card__subtitle' ) );
 
 		// Images should be rendered only for the first post.
 		$this->assertEquals( 1, substr_count( $content, 'class="mdc-card__media' ) );
@@ -144,8 +151,8 @@ class Test_Recent_Posts_Block extends \WP_UnitTestCase {
 		// Assert the article with featured image shows content inside the image container.
 		$this->assertEquals( 1, substr_count( $content, 'style="background-image: url(http://example.org/wp-content/uploads/image.jpg)"><div class="mdc-card__media-content">' ) );
 
-		// Assert all 5 posts are rendered.
-		$this->assertEquals( 5, substr_count( $content, 'class="single-post-card__title' ) );
+		// Assert all 4 posts are rendered.
+		$this->assertEquals( 4, substr_count( $content, 'class="single-post-card__title' ) );
 
 		$attributes['contentLayout'] = 'text-under-media';
 		$content                     = $block->render_block( $attributes );
