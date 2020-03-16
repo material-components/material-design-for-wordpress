7e8883796d479f0124d37a0a68f6d5d3cda7bfdb
David Lonjon <david.lonjon@gmail.com>
2020-03-16 16:43:17 +0900

    Fix unit test

--- tests/phpunit/php/customizer/class-test-range-slider-control.php
+++ tests/phpunit/php/customizer/class-test-range-slider-control.php
@@ -43,7 +43,7 @@ class Test_Range_Slider_Control extends \WP_UnitTestCase {
 		$output = ob_get_clean();
 
 		// Assert the Range Slider container is rendered
-		$this->assertContains( '<div class="mtb-ranger_slider" id="small_component_radius"></div>', $output );
+		$this->assertContains( '<div class="mtb-range_slider" id="small_component_radius"></div>', $output );
 	}
 
 	/**
