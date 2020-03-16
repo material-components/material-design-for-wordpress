3996ff7706497bb63cfe1f990879de5ec54c30fd
David Lonjon <david.lonjon@gmail.com>
2020-03-16 23:37:41 +0900

    Add e2e tests related to the Corner Styles customizer section

---
+++ tests/e2e/specs/customizer/customize-control.spec.js
@@ -0,0 +1,144 @@
+/**
+ * WordPress dependencies
+ */
+import { visitAdminPage } from '@wordpress/e2e-test-utils';
+
+const isVisible = async selector => {
+	return await page.$eval( selector, elem => {
+		return (
+			window.getComputedStyle( elem ).getPropertyValue( 'visibility' ) !==
+				'hidden' && elem.offsetHeight
+		);
+	} );
+};
+
+describe( 'Customize controls', () => {
+	beforeAll( async () => {
+		await visitAdminPage( 'customize.php' );
+	} );
+
+	describe( 'Corner Styles section', () => {
+		beforeAll( async () => {
+			await page.evaluate( el => {
+				el.click();
+			}, await page.$( '#accordion-panel-mtb h3' ) );
+
+			await page.waitFor( 500 );
+
+			await page.evaluate( el => {
+				el.click();
+			}, await page.$( '#accordion-section-mtb_corner_styles h3' ) );
+		} );
+
+		it( 'should always display the Small Component Radius setting control', async () => {
+			expect(
+				await isVisible( '#range-slider-control-mtb_small_component_radius' )
+			).toBeTruthy();
+		} );
+
+		it( 'should always display the Medium Component Radius setting control', async () => {
+			expect(
+				await isVisible( '#range-slider-control-mtb_medium_component_radius' )
+			).toBeTruthy();
+		} );
+
+		it( 'should always display the Large Component Radius setting control', async () => {
+			expect(
+				await isVisible( '#range-slider-control-mtb_large_component_radius' )
+			).toBeTruthy();
+		} );
+	} );
+
+	describe( 'Small Component Radius Control', () => {
+		beforeAll( async () => {
+			await page.evaluate( el => {
+				el.click();
+			}, await page.$( '#accordion-panel-mtb h3' ) );
+
+			await page.waitFor( 500 );
+
+			await page.evaluate( el => {
+				el.click();
+			}, await page.$( '#accordion-section-mtb_corner_styles h3' ) );
+		} );
+
+		it( 'should have a default value of 4 when the Baseline Design style is selected', async () => {
+			await page.evaluate( el => {
+				el.click();
+			}, await page.$( '#accordion-section-mtb_style h3' ) );
+
+			await page.evaluate(
+				radio => radio.click(),
+				await page.$( '#mtb_style-baseline' )
+			);
+
+			const inputValue = await page.evaluate( input => {
+				return input.value;
+			}, await page.$( '#range-slider-control-mtb_small_component_radius .components-range-control__number' ) );
+
+			expect( inputValue ).toBe( '4' );
+		} );
+	} );
+
+	describe( 'Medium Component Radius Control', () => {
+		beforeAll( async () => {
+			await page.evaluate( el => {
+				el.click();
+			}, await page.$( '#accordion-panel-mtb h3' ) );
+
+			await page.waitFor( 500 );
+
+			await page.evaluate( el => {
+				el.click();
+			}, await page.$( '#accordion-section-mtb_corner_styles h3' ) );
+		} );
+
+		it( 'should have a default value of 4 when the Baseline Design style is selected', async () => {
+			await page.evaluate( el => {
+				el.click();
+			}, await page.$( '#accordion-section-mtb_style h3' ) );
+
+			await page.evaluate(
+				radio => radio.click(),
+				await page.$( '#mtb_style-baseline' )
+			);
+
+			const inputValue = await page.evaluate( input => {
+				return input.value;
+			}, await page.$( '#range-slider-control-mtb_medium_component_radius .components-range-control__number' ) );
+
+			expect( inputValue ).toBe( '4' );
+		} );
+	} );
+
+	describe( 'Large Component Radius Control', () => {
+		beforeAll( async () => {
+			await page.evaluate( el => {
+				el.click();
+			}, await page.$( '#accordion-panel-mtb h3' ) );
+
+			await page.waitFor( 500 );
+
+			await page.evaluate( el => {
+				el.click();
+			}, await page.$( '#accordion-section-mtb_corner_styles h3' ) );
+		} );
+
+		it( 'should have a default value of 24 when the Baseline Design style is selected', async () => {
+			await page.evaluate( el => {
+				el.click();
+			}, await page.$( '#accordion-section-mtb_style h3' ) );
+
+			await page.evaluate(
+				radio => radio.click(),
+				await page.$( '#mtb_style-baseline' )
+			);
+
+			const inputValue = await page.evaluate( input => {
+				return input.value;
+			}, await page.$( '#range-slider-control-mtb_large_component_radius .components-range-control__number' ) );
+
+			expect( inputValue ).toBe( '24' );
+		} );
+	} );
+} );
--- tests/js/customizer/components/range-slider-control.test.js
+++ tests/js/customizer/components/range-slider-control.test.js
@@ -3,6 +3,7 @@
  */
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
+import { mount } from 'enzyme';
 
 /**
  * Internal dependencies
@@ -19,6 +20,18 @@ const setup = props => {
 	return render( <RangeSliderControl { ...props } /> );
 };
 
+/**
+ * Mount the component.
+ *
+ * @param {Object} props - Component props
+ * @return {Function} A functional component.
+ */
+const setupMount = props => {
+	return mount( <RangeSliderControl { ...props } /> );
+};
+
+const mockOnChangeFn = jest.fn();
+
 const baseProps = {
 	id: 'mtb-small_component_radius',
 	label: 'Small Component Radius',
@@ -27,12 +40,89 @@ const baseProps = {
 	min: 0,
 	max: 36,
 	step: 1,
-	onChange: () => null,
+	onChange: mockOnChangeFn,
 };
 
 describe( 'RangeSliderControl', () => {
+	beforeEach( () => {
+		jest.clearAllMocks();
+	} );
+
 	it( 'matches snapshot', () => {
 		const wrapper = setup( baseProps );
 		expect( wrapper ).toMatchSnapshot();
+	} );
+
+	it( 'should have its value changed after the input number field value is changed', () => {
+		const wrapper = setupMount( baseProps );
+
+		const input = wrapper.find( 'input.components-range-control__number' );
+
+		input.simulate( 'change', {
+			target: {
+				value: 5,
+				checkValidity: () => true,
+			},
+		} );
+
+		expect(
+			wrapper.find( 'input.components-range-control__number' ).prop( 'value' )
+		).toBe( 5 );
+
+		expect( mockOnChangeFn ).toHaveBeenCalledTimes( 1 );
+	} );
+
+	it( 'should have its value changed after the input range field value is changed', () => {
+		const wrapper = setupMount( baseProps );
+
+		const input = wrapper.find( 'input.components-range-control__slider' );
+
+		input.simulate( 'change', {
+			target: {
+				value: 4,
+				checkValidity: () => true,
+			},
+		} );
+
+		expect(
+			wrapper.find( 'input.components-range-control__slider' ).prop( 'value' )
+		).toBe( 4 );
+
+		expect( mockOnChangeFn ).toHaveBeenCalledTimes( 1 );
+	} );
+
+	it( 'should have its value reset to its original value after value being changed and reset', () => {
+		const wrapper = setupMount( baseProps );
+
+		const input = wrapper.find( 'input.components-range-control__slider' );
+
+		input.simulate( 'change', {
+			target: {
+				value: 3,
+				checkValidity: () => true,
+			},
+		} );
+
+		expect(
+			wrapper.find( 'input.components-range-control__slider' ).prop( 'value' )
+		).toBe( 3 );
+
+		wrapper.find( 'button' ).simulate( 'click' );
+
+		expect( mockOnChangeFn ).toHaveBeenCalledTimes( 2 );
+
+		expect(
+			wrapper.find( 'input.components-range-control__slider' ).prop( 'value' )
+		).toBe( 10 );
+	} );
+
+	it( 'should show the description when the title is clicked', () => {
+		const wrapper = setupMount( baseProps );
+
+		wrapper.find( '.range-slider-control-title' ).simulate( 'click' );
+
+		expect(
+			wrapper.find( '.customize-control-description' ).getDOMNode()
+		).toBeVisible();
 	} );
 } );
