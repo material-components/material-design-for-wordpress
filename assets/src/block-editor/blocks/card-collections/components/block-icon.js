e501916d9becc57be762195e8fc94518d91381e7
David Lonjon <david.lonjon@gmail.com>
2020-03-18 17:27:20 +0900

    WIP: Create a base Card Collection block

---
+++ assets/src/block-editor/blocks/card-collections/components/block-icon.js
@@ -0,0 +1,25 @@
+/**
+ * Block Icon component.
+ *
+ * @return {Function} A functional component.
+ */
+const BlockIcon = () => (
+	<svg
+		width="21"
+		height="21"
+		viewBox="0 0 21 21"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<rect
+			x="0.0537109"
+			y="0.856445"
+			width="20"
+			height="20"
+			rx="2"
+			fill="#555D67"
+		/>
+	</svg>
+);
+
+export default BlockIcon;
