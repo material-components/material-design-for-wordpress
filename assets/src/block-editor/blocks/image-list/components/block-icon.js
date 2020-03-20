733b7b801965a802334990ef621fd2d900207df8
Ravi Chandra <ravichandrach1@gmail.com>
2020-03-20 16:29:28 +0530

    Update block icon

--- assets/src/block-editor/blocks/image-list/components/block-icon.js
+++ assets/src/block-editor/blocks/image-list/components/block-icon.js
@@ -3,16 +3,6 @@
  *
  * @return {Function} A functional component.
  */
-const BlockIcon = () => (
-	<svg
-		width="18"
-		height="19"
-		viewBox="0 0 18 19"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<rect y="0.786133" width="18" height="18" fill="#555D66" />
-	</svg>
-);
+const BlockIcon = () => <span className="material-icons">collections</span>;
 
 export default BlockIcon;
