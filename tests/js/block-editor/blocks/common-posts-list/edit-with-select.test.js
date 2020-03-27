b54346a595c7ffe9ccadc4aa174db9c30c6f7b98
David Lonjon <david.lonjon@gmail.com>
2020-03-27 20:07:17 +0900

    Fix lint error

--- tests/js/block-editor/blocks/common-posts-list/edit-with-select.test.js
+++ tests/js/block-editor/blocks/common-posts-list/edit-with-select.test.js
@@ -4,7 +4,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import { registerStore } from '@wordpress/data';
-import { pickBy } from 'lodash';
+
 /**
  * Internal dependencies
  */
