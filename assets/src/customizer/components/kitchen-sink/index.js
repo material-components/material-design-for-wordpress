6688e75f5b73d6186d993ebfdabe9546925ef0a9
Dukagjin Surdulli <me@dukagj.in>
2020-03-30 16:13:17 -0400

    Pass customizer settings as props to the KitchenSink component

--- assets/src/customizer/components/kitchen-sink/index.js
+++ assets/src/customizer/components/kitchen-sink/index.js
@@ -1,3 +1,30 @@
-const KitchenSink = () => <h1>Kitchenn Sink Boi</h1>;
+/*
+{
+	bodyFontFamily,
+	headFontFamily,
+	iconCollection,
+	largeComponentRadius,
+	mediumComponentRadius,
+	primaryColor,
+	primaryTextColor,
+	secondaryColor,
+	secondaryTextColor,
+	smallComponentRadius,
+	style,
+}
+*/
+
+const KitchenSink = props => (
+	<div>
+		<h1>Kitchenn Sink Boi</h1>
+		<ul>
+			{ Object.keys( props ).map( prop => (
+				<li key={ prop }>
+					{ prop }: { props[ prop ] }
+				</li>
+			) ) }
+		</ul>
+	</div>
+);
 
 export default KitchenSink;
