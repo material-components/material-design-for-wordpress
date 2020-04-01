77f393560d16f27fbc27d9248e7c2b1f5d089c26
David Lonjon <david.lonjon@gmail.com>
2020-04-01 19:06:05 +0900

    Refactor the card inspector controls panel in collection

--- assets/src/block-editor/blocks/card-collections/components/inspector-controls.js
+++ assets/src/block-editor/blocks/card-collections/components/inspector-controls.js
@@ -4,7 +4,7 @@
 import { __ } from '@wordpress/i18n';
 import { InspectorControls } from '@wordpress/block-editor';
 import { useCallback } from '@wordpress/element';
-import { PanelBody, RangeControl } from '@wordpress/components';
+import { PanelBody, RangeControl, ToggleControl } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -12,6 +12,7 @@ import { PanelBody, RangeControl } from '@wordpress/components';
 import genericAttributesSetter from '../../../utils/generic-attributes-setter';
 import CardStylesPanel from '../../../components/card-styles-panel';
 import InspectorControlsStylePanel from '../../card/components/inspector-controls-style-panel';
+import InspectorControlsContentPanel from '../../card/components/inspector-controls-content-panel';
 
 import { MIN_NUMBER_OF_CARDS, MAX_NUMBER_OF_CARDS } from '../constants';
 
@@ -33,20 +34,37 @@ const CardCollectionsInspectorControls = ( { attributes, setAttributes } ) => {
 	const {
 		style,
 		columns,
+		contentLayout,
 		numberOfCards,
 		cardsProps,
 		gutter,
 		cornerRadius,
 		outlined,
+		allowIndividualStyleOverride,
+		allowIndividualContentOverride,
+		displayTitle,
+		displaySubTitle,
+		displaySecondaryText,
+		displayActions,
+		displayImage,
 		setter,
 	} = attributes;
+
+	let showContentLayout = true;
+
+	if ( style === 'list' ) {
+		showContentLayout = false;
+	}
 
 	return (
 		<InspectorControls>
 			<CardStylesPanel
 				style={ style }
 				columns={ columns }
-				showContentLayout={ false }
+				allowIndividualStyleOverride={ allowIndividualStyleOverride }
+				showAllowIndividualStyleOverride={ true }
+				contentLayout={ contentLayout }
+				showContentLayout={ showContentLayout }
 				gutter={ gutter }
 				showGutter={ true }
 				roundedCorners={ cornerRadius }
@@ -65,16 +83,52 @@ const CardCollectionsInspectorControls = ( { attributes, setAttributes } ) => {
 					min={ MIN_NUMBER_OF_CARDS }
 					max={ MAX_NUMBER_OF_CARDS }
 				/>
+
+				<ToggleControl
+					label={ __(
+						'Allow individual card override',
+						'material-theme-builder'
+					) }
+					checked={ allowIndividualContentOverride }
+					onChange={ standardSetter( 'allowIndividualContentOverride' ) }
+				/>
+
+				{ ! allowIndividualContentOverride && (
+					<>
+						<ToggleControl
+							label={ __( 'Show title', 'material-theme-builder' ) }
+							checked={ displayTitle }
+							onChange={ standardSetter( 'displayTitle' ) }
+						/>
+						<ToggleControl
+							label={ __( 'Show Subtitle', 'material-theme-builder' ) }
+							checked={ displaySubTitle }
+							onChange={ standardSetter( 'displaySubTitle' ) }
+						/>
+						<ToggleControl
+							label={ __( 'Show Image', 'material-theme-builder' ) }
+							checked={ displayImage }
+							onChange={ standardSetter( 'displayImage' ) }
+						/>
+						{ style !== 'list' && (
+							<ToggleControl
+								label={ __( 'Show Secondary Text', 'material-theme-builder' ) }
+								checked={ displaySecondaryText }
+								onChange={ standardSetter( 'displaySecondaryText' ) }
+							/>
+						) }
+						<ToggleControl
+							label={ __( 'Show Actions', 'material-theme-builder' ) }
+							checked={ displayActions }
+							onChange={ standardSetter( 'displayActions' ) }
+						/>
+					</>
+				) }
 			</PanelBody>
 			{ cardsProps.map( ( cardProps, cardIndex ) => {
 				const inspectorControlsStylePanelProps = {
 					cardLayoutStyle: style === 'list' ? 'horizontal' : 'vertical',
 					contentLayout: cardProps.contentLayout,
-					displayTitle: cardProps.displayTitle,
-					displaySubTitle: cardProps.displaySubTitle,
-					displayImage: cardProps.displayImage,
-					displaySecondaryText: cardProps.displaySecondaryText,
-					displayActions: cardProps.displayActions,
 					cornerRadius: cardProps.cornerRadius,
 					outlined: cardProps.outlined,
 					isSingleCard: false,
@@ -82,11 +136,31 @@ const CardCollectionsInspectorControls = ( { attributes, setAttributes } ) => {
 					cardIndex,
 					panelsInitialOpen: false,
 				};
+
+				const inspectorControlsContentPanelProps = {
+					displayTitle: cardProps.displayTitle,
+					displaySubTitle: cardProps.displaySubTitle,
+					displayImage: cardProps.displayImage,
+					displaySecondaryText: cardProps.displaySecondaryText,
+					displayActions: cardProps.displayActions,
+					isSingleCard: false,
+					setter,
+					cardIndex,
+					panelsInitialOpen: false,
+				};
 				return (
-					<InspectorControlsStylePanel
-						key={ cardIndex }
-						{ ...inspectorControlsStylePanelProps }
-					/>
+					<div key={ cardIndex }>
+						{ allowIndividualStyleOverride && (
+							<InspectorControlsStylePanel
+								{ ...inspectorControlsStylePanelProps }
+							/>
+						) }
+						{ allowIndividualContentOverride && (
+							<InspectorControlsContentPanel
+								{ ...inspectorControlsContentPanelProps }
+							/>
+						) }
+					</div>
 				);
 			} ) }
 		</InspectorControls>
