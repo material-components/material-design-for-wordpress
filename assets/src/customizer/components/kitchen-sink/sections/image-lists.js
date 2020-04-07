972680730100de90f25da5e3d63f92182b4d04fe
Dukagjin Surdulli <me@dukagj.in>
2020-04-07 17:51:49 -0400

    Add the image list component

--- assets/src/customizer/components/kitchen-sink/sections/image-lists.js
+++ assets/src/customizer/components/kitchen-sink/sections/image-lists.js
@@ -1,191 +1,310 @@
 import { H3 } from '../styles';
 
-const ImageLists = () => (
+const ImageLists = ( { radius } ) => (
 	<div>
 		<H3>Image Lists</H3>
 		<p>This component can be created as a block in WordPress.</p>
-		<ul className="mdc-image-list standard-image-list mdc-image-list--with-text-protection">
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+		<div>
+			<ul className="mdc-image-list mdc-image-list--with-text-protection">
+				<li className="mdc-image-list__item image-list-item">
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/1.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+				<li
+					className="mdc-image-list__item image-list-item"
+					style={ { borderRadius: `${ radius }px` } }
+				>
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+				<li
+					className="mdc-image-list__item image-list-item"
+					style={ { borderRadius: `${ radius }px` } }
+				>
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/3.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+				<li
+					className="mdc-image-list__item image-list-item"
+					style={ { borderRadius: `${ radius }px` } }
+				>
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/4.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+				<li
+					className="mdc-image-list__item image-list-item"
+					style={ { borderRadius: `${ radius }px` } }
+				>
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/5.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+				<li
+					className="mdc-image-list__item image-list-item"
+					style={ { borderRadius: `${ radius }px` } }
+				>
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/6.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+				<li
+					className="mdc-image-list__item image-list-item"
+					style={ { borderRadius: `${ radius }px` } }
+				>
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/7.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+				<li
+					className="mdc-image-list__item image-list-item"
+					style={ { borderRadius: `${ radius }px` } }
+				>
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/8.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+				<li
+					className="mdc-image-list__item image-list-item"
+					style={ { borderRadius: `${ radius }px` } }
+				>
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/9.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+				<li
+					className="mdc-image-list__item image-list-item"
+					style={ { borderRadius: `${ radius }px` } }
+				>
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/10.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+				<li
+					className="mdc-image-list__item image-list-item"
+					style={ { borderRadius: `${ radius }px` } }
+				>
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/11.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+				<li
+					className="mdc-image-list__item image-list-item"
+					style={ { borderRadius: `${ radius }px` } }
+				>
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/12.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+				<li
+					className="mdc-image-list__item image-list-item"
+					style={ { borderRadius: `${ radius }px` } }
+				>
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/13.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+				<li
+					className="mdc-image-list__item image-list-item"
+					style={ { borderRadius: `${ radius }px` } }
+				>
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/14.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-			<li className="mdc-image-list__item">
-				<div className="mdc-image-list__image-aspect-container">
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+				<li
+					className="mdc-image-list__item image-list-item"
+					style={ { borderRadius: `${ radius }px` } }
+				>
 					<img
+						style={ { borderRadius: `${ radius }px` } }
 						className="mdc-image-list__image"
 						src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/15.jpg"
 						alt="Text label"
 					/>
-				</div>
-				<div className="mdc-image-list__supporting">
-					<span className="mdc-image-list__label">Text label</span>
-				</div>
-			</li>
-		</ul>
+					<div
+						className="mdc-image-list__supporting"
+						style={ {
+							borderBottomLeftRadius: `${ radius }px`,
+							borderBottomRightRadius: `${ radius }px`,
+						} }
+					>
+						<span className="mdc-image-list__label">Text label</span>
+					</div>
+				</li>
+			</ul>
+		</div>
 	</div>
 );
 
