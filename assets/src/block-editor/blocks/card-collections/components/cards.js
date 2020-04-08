d821f282e09103cbe0d144a7e623327b44b51b97
David Lonjon <david.lonjon@gmail.com>
2020-04-08 15:35:39 +0900

    Remove an unnecessary return statement

--- assets/src/block-editor/blocks/card-collections/components/cards.js
+++ assets/src/block-editor/blocks/card-collections/components/cards.js
@@ -7,26 +7,24 @@ import Masonry from 'react-masonry-css';
  * @param {Array} props.cards - Cards HTML markup.
  * @return {Function} Function returning the HTML markup for the component.
  */
-const Cards = ( { style, columns, cards } ) => {
-	return (
-		<>
-			{ ( style === 'grid' || style === 'list' ) && (
-				<div className={ `mdc-layout-grid layout-${ style }` }>
-					<div className="mdc-layout-grid__inner">{ cards }</div>
-				</div>
-			) }
+const Cards = ( { style, columns, cards } ) => (
+	<>
+		{ ( style === 'grid' || style === 'list' ) && (
+			<div className={ `mdc-layout-grid layout-${ style }` }>
+				<div className="mdc-layout-grid__inner">{ cards }</div>
+			</div>
+		) }
 
-			{ style === 'masonry' && (
-				<Masonry
-					breakpointCols={ columns }
-					className={ `masonry-grid layout-${ style }` }
-					columnClassName="masonry-grid_column"
-				>
-					{ cards }
-				</Masonry>
-			) }
-		</>
-	);
-};
+		{ style === 'masonry' && (
+			<Masonry
+				breakpointCols={ columns }
+				className={ `masonry-grid layout-${ style }` }
+				columnClassName="masonry-grid_column"
+			>
+				{ cards }
+			</Masonry>
+		) }
+	</>
+);
 
 export default Cards;
