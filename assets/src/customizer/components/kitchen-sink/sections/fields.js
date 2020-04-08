13e90263701e7ab8bc57d674019037d450793cc3
Dukagjin Surdulli <me@dukagj.in>
2020-04-08 16:02:02 -0400

    Display all components correctly and fix textfield corner bug

--- assets/src/customizer/components/kitchen-sink/sections/fields.js
+++ assets/src/customizer/components/kitchen-sink/sections/fields.js
@@ -25,19 +25,31 @@ const Fields = ( { radius } ) => (
 
 			<div
 				className="mdc-text-field mdc-text-field--outlined mdc-text-field--with-leading-icon mdc-text-field--with-trailing-icon"
-				style={ { marginLeft: '10px', borderRadius: `${ radius }px` } }
+				style={ { marginLeft: '10px' } }
 			>
 				<i className="material-icons mdc-text-field__icon">account_circle</i>
 				<i className="material-icons mdc-text-field__icon">close</i>
 				<input className="mdc-text-field__input" type="text" />
 				<div className="mdc-notched-outline">
-					<div className="mdc-notched-outline__leading"></div>
+					<div
+						className="mdc-notched-outline__leading"
+						style={ {
+							borderRadius: `${ radius }px 0 0 ${ radius }px`,
+							minWidth: `${ radius }px`,
+						} }
+					></div>
 					<div className="mdc-notched-outline__notch">
 						<label htmlFor="outlined-textfield" className="mdc-floating-label">
 							Last name
 						</label>
 					</div>
-					<div className="mdc-notched-outline__trailing"></div>
+					<div
+						className="mdc-notched-outline__trailing"
+						style={ {
+							borderRadius: `0 ${ radius }px ${ radius }px 0`,
+							minWidth: `${ radius }px`,
+						} }
+					></div>
 				</div>
 			</div>
 		</div>
