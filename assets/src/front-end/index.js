837810c76295c7e453a4f7da595eba62efb8a792
Dukagjin Surdulli <me@dukagj.in>
2020-04-09 15:05:22 -0400

    Refactor frontend init functions for mdc blocks

--- assets/src/front-end/index.js
+++ assets/src/front-end/index.js
@@ -1,58 +1,11 @@
 /* global addEventListener */
 /* istanbul ignore file */
 
-/**
- * External dependencies
- */
-import { MDCRipple } from '@material/ripple';
-import { MDCList } from '@material/list';
-import { MDCTabBar } from '@material/tab-bar';
-
-/**
- * Internal dependencies
- */
-import '../block-editor/blocks/hello-world/save.css';
-
-const initButtons = () => {
-	const buttons = document.querySelectorAll( '.mdc-button' );
-	const iconButtons = document.querySelectorAll( '.mdc-icon-button' );
-
-	buttons.forEach( button => new MDCRipple( button ) );
-	iconButtons.forEach( button => ( new MDCRipple( button ).unbounded = true ) );
-};
-
-const initLists = () => {
-	const lists = document.querySelectorAll( '.mdc-list' );
-
-	lists.forEach( list => {
-		const mdcList = new MDCList( list );
-		mdcList.listElements.forEach( listItemEl => new MDCRipple( listItemEl ) );
-	} );
-};
-
-const initTabBar = () => {
-	const tabBars = document.querySelectorAll( '.mdc-tab-bar-container' );
-
-	tabBars.forEach( tabBarContainer => {
-		const tabBar = new MDCTabBar(
-			tabBarContainer.querySelector( '.mdc-tab-bar' )
-		);
-
-		const contentElements = tabBarContainer.querySelectorAll(
-			'.mdc-tab-content'
-		);
-
-		tabBar.listen( 'MDCTabBar:activated', event => {
-			tabBarContainer
-				.querySelector( '.mdc-tab-content--active' )
-				.classList.remove( 'mdc-tab-content--active' );
-
-			contentElements[ event.detail.index ].classList.add(
-				'mdc-tab-content--active'
-			);
-		} );
-	} );
-};
+import {
+	initButtons,
+	initLists,
+	initTabBar,
+} from '../common/mdc-components-init';
 
 addEventListener( 'DOMContentLoaded', () => {
 	initButtons();
