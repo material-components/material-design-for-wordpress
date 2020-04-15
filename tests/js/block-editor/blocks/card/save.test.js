50f5329043835a7c0047d213026808c37c954833
David Lonjon <david.lonjon@gmail.com>
2020-04-15 14:16:15 +0900

    Add JS unit tests for the Card Block components

---
+++ tests/js/block-editor/blocks/card/save.test.js
@@ -0,0 +1,69 @@
+/**
+ * External dependencies
+ */
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+
+/**
+ * Internal dependencies
+ */
+import Save from '../../../../../assets/src/block-editor/blocks/card/save';
+
+/**
+ * Render the component.
+ *
+ * @param {Object} props - Component props.
+ * @return {Function} A functional component.
+ */
+const setup = props => {
+	return render( <Save { ...props } /> );
+};
+
+const baseProps = {
+	attributes: {
+		cardLayout: 'vertical',
+		contentLayout: 'text-under-media',
+		title: 'Test title',
+		displayTitle: true,
+		secondaryText: 'Test secondary text',
+		displaySecondaryText: true,
+		imageSourceUrl: 'http://test.loc/test.jpg',
+		isImageEditMode: false,
+		displayImage: true,
+		supportingText: 'Test supporting text',
+		displaySupportingText: true,
+		primaryActionButtonLabel: 'Test button action 1',
+		primaryActionButtonUrl: 'http://test.loc/hello-world',
+		primaryActionButtonNewTab: true,
+		primaryActionButtonNoFollow: true,
+		secondaryActionButtonLabel: 'Test button action 2',
+		secondaryActionButtonUrl: 'http://test.loc/hello-world-again',
+		secondaryActionButtonNewTab: true,
+		secondaryActionButtonNoFollow: true,
+		displayActions: true,
+		displaySecondaryActionButton: true,
+		outlined: true,
+		cornerRadius: 4,
+	},
+};
+
+describe( 'blocks: material/card: Save', () => {
+	it( 'matches snapshot', () => {
+		const wrapper = setup( baseProps );
+		expect( wrapper ).toMatchSnapshot();
+	} );
+
+	it( 'matches snapshot with a `horizontal` card layout.', () => {
+		const props = { ...baseProps };
+		baseProps.attributes.cardLayout = 'horizontal';
+		const wrapper = setup( props );
+		expect( wrapper ).toMatchSnapshot();
+	} );
+
+	it( 'matches snapshot when props get defaults', () => {
+		const props = { ...baseProps };
+		delete props.attributes.cardLayout;
+		const wrapper = setup( props );
+		expect( wrapper ).toMatchSnapshot();
+	} );
+} );
