3add9f54932058d8fdd6996c1da5a86f7aca33b1
David Lonjon <david.lonjon@gmail.com>
2020-04-17 21:17:46 +0900

    Refactor the button save component to allow for a submit button

--- assets/src/block-editor/blocks/button/save.js
+++ assets/src/block-editor/blocks/button/save.js
@@ -8,6 +8,33 @@ import classNames from 'classnames';
  */
 import hasBg from './utils/has-bg';
 
+/**
+ * Button Children component.
+ *
+ * @param {Object} props - Component props.
+ * @param {string} props.icon - Button icon name.
+ * @param {string} props.iconPosition - Button icon position.
+ * @param {string} props.label - Button label.
+ *
+ * @return {Function} Function returning the HTML markup for the component.
+ */
+const ButtonChildren = ( { icon, iconPosition, label } ) => (
+	<>
+		{ icon && iconPosition === 'leading' && (
+			<i className="material-icons mdc-button__icon">
+				{ String.fromCharCode( icon?.hex ) }
+			</i>
+		) }
+		<div className="mdc-button__ripple"></div>
+		<span className="mdc-button__label">{ label }</span>
+		{ icon && iconPosition === 'trailing' && (
+			<i className="material-icons mdc-button__icon">
+				{ String.fromCharCode( icon?.hex ) }
+			</i>
+		) }
+	</>
+);
+
 const ButtonSave = ( {
 	attributes: {
 		rel,
@@ -28,52 +55,74 @@ const ButtonSave = ( {
 	if ( 'icon' === type ) {
 		return (
 			<div className={ className }>
-				<a
-					href={ url && ! isSubmit ? url : '#' }
-					rel={ rel && ! isSubmit ? rel : undefined }
-					target={ linkTarget && ! isSubmit ? linkTarget : undefined }
-					className={ classNames( 'material-icons', 'mdc-icon-button', {
-						'is-submit': isSubmit,
-					} ) }
-					style={ { ...( textColor ? { color: textColor } : {} ) } }
-				>
-					{ String.fromCharCode( icon?.hex ) }
-				</a>
+				{ url && ! isSubmit ? (
+					<a
+						href={ url }
+						rel={ rel && ! isSubmit ? rel : undefined }
+						target={ linkTarget && ! isSubmit ? linkTarget : undefined }
+						className={ classNames( 'material-icons', 'mdc-icon-button' ) }
+						style={ { ...( textColor ? { color: textColor } : {} ) } }
+					>
+						{ String.fromCharCode( icon?.hex ) }
+					</a>
+				) : (
+					<button
+						className="material-icons mdc-icon-button"
+						style={ { ...( textColor ? { color: textColor } : {} ) } }
+						type={ isSubmit ? 'submit' : undefined }
+					>
+						{ String.fromCharCode( icon?.hex ) }
+					</button>
+				) }
 			</div>
 		);
 	}
 
 	return (
 		<div className={ className }>
-			<a
-				href={ url && ! isSubmit ? url : '#' }
-				rel={ rel && ! isSubmit ? rel : undefined }
-				target={ linkTarget && ! isSubmit ? linkTarget : undefined }
-				style={ {
-					...( backgroundColor && hasBg( style ) ? { backgroundColor } : {} ),
-					...( textColor ? { color: textColor } : {} ),
-					...( cornerRadius !== undefined
-						? { borderRadius: `${ cornerRadius }px` }
-						: {} ),
-				} }
-				className={ classNames( 'mdc-button', {
-					'is-submit': isSubmit,
-					[ `mdc-button--${ style }` ]: true,
-				} ) }
-			>
-				{ icon && iconPosition === 'leading' && (
-					<i className="material-icons mdc-button__icon">
-						{ String.fromCharCode( icon?.hex ) }
-					</i>
-				) }
-				<div className="mdc-button__ripple"></div>
-				<span className="mdc-button__label">{ label }</span>
-				{ icon && iconPosition === 'trailing' && (
-					<i className="material-icons mdc-button__icon">
-						{ String.fromCharCode( icon?.hex ) }
-					</i>
-				) }
-			</a>
+			{ url && ! isSubmit ? (
+				<a
+					href={ url }
+					rel={ rel && ! isSubmit ? rel : undefined }
+					target={ linkTarget && ! isSubmit ? linkTarget : undefined }
+					style={ {
+						...( backgroundColor && hasBg( style ) ? { backgroundColor } : {} ),
+						...( textColor ? { color: textColor } : {} ),
+						...( cornerRadius !== undefined
+							? { borderRadius: `${ cornerRadius }px` }
+							: {} ),
+					} }
+					className={ classNames( 'mdc-button', {
+						[ `mdc-button--${ style }` ]: true,
+					} ) }
+				>
+					<ButtonChildren
+						icon={ icon }
+						iconPosition={ iconPosition }
+						label={ label }
+					/>
+				</a>
+			) : (
+				<button
+					className={ classNames( 'mdc-button', {
+						[ `mdc-button--${ style }` ]: true,
+					} ) }
+					style={ {
+						...( backgroundColor && hasBg( style ) ? { backgroundColor } : {} ),
+						...( textColor ? { color: textColor } : {} ),
+						...( cornerRadius !== undefined
+							? { borderRadius: `${ cornerRadius }px` }
+							: {} ),
+					} }
+					type={ isSubmit ? 'submit' : undefined }
+				>
+					<ButtonChildren
+						icon={ icon }
+						iconPosition={ iconPosition }
+						label={ label }
+					/>
+				</button>
+			) }
 		</div>
 	);
 };
