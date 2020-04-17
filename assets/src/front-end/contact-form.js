1bcdc85da2626699967d302abdbf3bd959ff56cd
David Lonjon <david.lonjon@gmail.com>
2020-04-17 21:18:26 +0900

    Refactor the contact form handling

--- assets/src/front-end/contact-form.js
+++ assets/src/front-end/contact-form.js
@@ -1,27 +1,25 @@
-/* global grecaptcha, mtb, jQuery */
+/* global grecaptcha, mtb, fetch, FormData */
 
 /**
  * External dependencies
  */
 import { MDCTextField } from '@material/textfield';
 
-const $ = jQuery.noConflict( true );
-
 /**
- *
- * @param {*} form
+ * Initialize contact form.
  */
 export const initContactForm = () => {
-	const form = document.getElementById( 'mtb-contactForm' );
-
+	const form = document.getElementById( 'mtbContactForm' );
 	if ( ! form ) {
-		return;
+		return false;
 	}
 
-	const fields = [];
+	const contactFormMessage = document.getElementById(
+		'mtbContactFormMsgContainer'
+	);
 
 	for ( const field of form.querySelectorAll( '.mdc-text-field' ) ) {
-		fields.push( new MDCTextField( field ) );
+		new MDCTextField( field );
 	}
 
 	initReCaptchaToken();
@@ -29,75 +27,81 @@ export const initContactForm = () => {
 	form.addEventListener( 'submit', event => {
 		event.preventDefault();
 
-		let isValid = true;
-		fields.forEach( field => {
-			if ( field.required && ! field.valid ) {
-				isValid = false;
-				// Blur the field so error styles are rendered.
-				field.foundation_.deactivateFocus();
-			}
-		} );
-
-		if ( ! isValid ) {
-			event.preventDefault();
-			return;
-		}
-
-		const data = {
-			token: form.querySelector( '[name=mtb_token]' ).value,
-			contactFields: {},
-		};
+		const contactFields = {};
+		const formData = new FormData();
+		formData.append( 'token', form.querySelector( '[name=mtb_token]' ).value );
 
 		for ( const field of form.querySelectorAll( 'input, textarea' ) ) {
 			const name = field.name;
 			const label = field.dataset.label;
 			const value = field.value;
 
-			if ( field.type !== 'hidden' ) {
-				data.contactFields[ field.id ] = {
+			if ( field.type !== 'hidden' && field.type !== 'submit' ) {
+				contactFields[ field.id ] = {
 					name,
 					label,
 					value,
 				};
 			} else {
-				data[ name ] = value;
+				formData.append( name, value );
 			}
 		}
 
-		$.ajax( {
-			type: 'POST',
-			url: mtb.ajax_url,
-			data,
-			dataType: 'json',
-			success( response ) {
-				// console.log( response );
-				if ( response.success === true ) {
-					form[ 0 ].reset();
-					form.hide();
-					$( '#contactFormMsg' ).show();
+		formData.append( 'contact_fields', JSON.stringify( contactFields ) );
+
+		fetch( mtb.ajax_url, {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+			},
+			body: new URLSearchParams( formData ),
+		} )
+			.then( response => response.json() )
+			.then( data => {
+				if ( data.success === true ) {
+					form.reset();
+					form.style.display = 'none';
+					contactFormMessage.style.display = 'block';
 					initReCaptchaToken();
 				} else {
 					// TODO: Refactor to show message in page.
 					// eslint-disable-next-line no-alert,no-undef
 					alert( 'An error occurred' );
 				}
-			},
-		} );
+			} )
+			.catch( () => {
+				// TODO: Refactor to show message in page.
+				// eslint-disable-next-line no-alert,no-undef
+				alert( 'An error occurred' );
+			} );
+
+		return false;
 	} );
 
-	$( '#contactFormMsg' ).on( 'click', function() {
-		$( '#contactFormMsg' ).hide();
-		form.show();
+	for ( const field of form.querySelectorAll( 'input, textarea' ) ) {
+		field.addEventListener(
+			'invalid',
+			() => {
+				field
+					.closest( '.mdc-text-field' )
+					.classList.add( 'mdc-text-field--invalid' );
+			},
+			false
+		);
+	}
+
+	contactFormMessage.addEventListener( 'click', () => {
+		contactFormMessage.style.display = 'none';
+		form.style.display = 'block';
 	} );
 };
 
 /**
  * Initialize reCAPTCHA token.
- *
- * @param field
  */
 export const initReCaptchaToken = () => {
-	const form = document.getElementById( 'mtb-contactForm' );
+	const form = document.getElementById( 'mtbContactForm' );
 
 	if ( ! form ) {
 		return;
--- php/templates/contact-form.php
+++ php/templates/contact-form.php
@@ -12,13 +12,13 @@ $form_allowed_tags = isset( $form_allowed_tags ) ? $form_allowed_tags : wp_kses_
 $class_name        = isset( $attributes['className'] ) ? $attributes['className'] : '';
 ?>
 
-<form id="contactForm">
+<form id="mtbContactForm" action="javascript:void(0);">
 	<?php echo wp_kses( $content, $form_allowed_tags ); ?>
 	<input type="hidden" name="action" value="mtb_submit_contact_form"/>
 	<input type="hidden" name="mtb_token" value="token_here"/>
 	<?php wp_nonce_field( 'contact_form_action', 'mtb_contact_form_nonce' ); ?>
 </form>
-<div id="contactFormMsg" style="display: none
+<div id="mtbContactFormMsgContainer" style="display: none
 ">
 	<?php esc_html_e( 'Your request has been successfully submitted', 'material-theme-builder' ); ?>
 	<br/>
