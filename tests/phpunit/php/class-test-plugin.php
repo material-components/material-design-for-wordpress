9f295e5b76dcc8e8cb52b71fc904d8cb942fb9b3
David Lonjon <david.lonjon@gmail.com>
2020-04-19 23:55:58 +0900

    Fix PHP unit tests

--- tests/phpunit/php/class-test-plugin.php
+++ tests/phpunit/php/class-test-plugin.php
@@ -61,6 +61,17 @@ class Test_Plugin extends \WP_UnitTestCase {
 	}
 
 	/**
+	 * Test for enqueue_block_editor_assets() method.
+	 *
+	 * @see Plugin::enqueue_google_fonts()
+	 */
+	public function test_enqueue_google_fonts() {
+		$plugin = get_plugin_instance();
+		$plugin->enqueue_google_fonts();
+		$this->assertTrue( wp_style_is( 'material-google-fonts-cdn', 'enqueued' ) );
+	}
+
+	/**
 	 * Test for enqueue_front_end_assets() method.
 	 *
 	 * @see Plugin::enqueue_front_end_assets()
@@ -71,7 +82,6 @@ class Test_Plugin extends \WP_UnitTestCase {
 		$plugin = get_plugin_instance();
 		$plugin->enqueue_front_end_assets();
 		$this->assertTrue( wp_script_is( 'material-front-end-js', 'enqueued' ) );
-		$this->assertTrue( wp_style_is( 'material-google-fonts-cdn', 'enqueued' ) );
 		$this->assertTrue( wp_style_is( 'material-front-end-css', 'enqueued' ) );
 		$this->assertFalse( wp_style_is( 'material-overrides-css', 'enqueued' ) );
 
