53e894b00f789d1c3216d7713673041dee921bc7
David Lonjon <david.lonjon@gmail.com>
2020-04-22 15:31:27 +0900

    Add PHP unit tests

--- tests/phpunit/php/class-test-plugin.php
+++ tests/phpunit/php/class-test-plugin.php
@@ -15,6 +15,45 @@ use MaterialThemeBuilder\Customizer\Controls;
 class Test_Plugin extends \WP_UnitTestCase {
 
 	/**
+	 * Created PostID.
+	 *
+	 * @var int
+	 */
+	public static $post_id = 0;
+
+	/**
+	 * Generate some text fixtures.
+	 *
+	 * @param WP_UnitTest_Factory $factory WP Factory object.
+	 *
+	 * @return void
+	 */
+	public static function wpSetUpBeforeClass( $factory ) {
+		self::generate_fixtures( $factory );
+	}
+
+	/**
+	 * Helper method to generate the fixtures
+	 *
+	 * @param WP_UnitTest_Factory $factory WP Factory object.
+	 *
+	 * @return void
+	 */
+	public static function generate_fixtures( $factory ) {
+		self::$post_id = $factory->post->create(
+			[
+				'post_title'   => 'Lorem ipsum dolor sit amet',
+				'post_content' => 'Consectetur adipiscing elit. In dui quam, egestas nec aliquet ac, hendrerit vitae ligula. Morbi malesuada in lectus vel sollicitudin. Proin tellus ligula, tincidunt at sagittis eget, tempor non est. In et suscipit metus. Cras in lectus a ex ullamcorper eleifend. Aenean convallis lacus et porttitor convallis. Proin iaculis a diam et euismod. Proin lectus ex, bibendum vel pretium ut, pellentesque eget nisl.
+
+				<!-- wp:material/contact-form {"gutter":{"desktop":24,"tablet":18,"mobile":12},"displayLightbox":true,"align":"wide"} -->
+					<div class="wp-block-material-contact-form" id="block-material-contact-form-1"></div>
+				<!-- /wp:material/contact-form -->
+				',
+			]
+		);
+	}
+
+	/**
 	 * Test constructor.
 	 *
 	 * @see Plugin::__construct()
@@ -49,6 +88,9 @@ class Test_Plugin extends \WP_UnitTestCase {
 	 * @see Plugin::enqueue_block_editor_assets()
 	 */
 	public function test_enqueue_block_editor_assets() {
+		$user_id = self::factory()->user->create( [ 'role' => 'editor' ] );
+		wp_set_current_user( $user_id );
+
 		$plugin = get_plugin_instance();
 		$plugin->enqueue_block_editor_assets();
 		$this->assertTrue( wp_script_is( 'material-block-editor-js', 'enqueued' ) );
@@ -58,6 +100,12 @@ class Test_Plugin extends \WP_UnitTestCase {
 		// Assert inline css vars are added.
 		$inline_css = wp_styles()->get_data( 'material-block-editor-css', 'after' );
 		$this->assertNotEmpty( $inline_css );
+
+		$inline_js = wp_scripts()->get_data( 'material-block-editor-js', 'data' );
+		// Assert inline css vars contains ajax url data.
+		$this->assertRegexp( '/ajax_url/', $inline_js );
+		// Assert inline css vars contains data specific when a user is logged as editor or administrator.
+		$this->assertRegexp( '/recaptcha_site_key/', $inline_js );
 	}
 
 	/**
@@ -77,17 +125,31 @@ class Test_Plugin extends \WP_UnitTestCase {
 	 * @see Plugin::enqueue_front_end_assets()
 	 */
 	public function test_enqueue_front_end_assets() {
+		global $post;
+
 		add_filter( 'stylesheet', [ $this, 'stylesheet' ] );
 
+		$post = get_post( self::$post_id );
+		update_option( 'mtb_recaptcha_site_key', 'test-key' );
+		update_option( 'mtb_recaptcha_client_secret', 'test-secret' );
+
 		$plugin = get_plugin_instance();
+
 		$plugin->enqueue_front_end_assets();
 		$this->assertTrue( wp_script_is( 'material-front-end-js', 'enqueued' ) );
+		$this->assertTrue( wp_script_is( 'google-recaptcha-v3', 'enqueued' ) );
 		$this->assertTrue( wp_style_is( 'material-front-end-css', 'enqueued' ) );
 		$this->assertFalse( wp_style_is( 'material-overrides-css', 'enqueued' ) );
 
 		// Assert inline css vars are added.
 		$inline_css = wp_styles()->get_data( 'material-front-end-css', 'after' );
 		$this->assertNotEmpty( $inline_css );
+
+		$inline_js = wp_scripts()->get_data( 'material-block-editor-js', 'data' );
+		// Assert inline css vars contains ajax url data.
+		$this->assertRegexp( '/ajax_url/', $inline_js );
+		// Assert inline css vars contains data specific when a post contains the material contact form block.
+		$this->assertRegexp( '/recaptcha_site_key/', $inline_js );
 
 		remove_filter( 'stylesheet', [ $this, 'stylesheet' ] );
 		$plugin->enqueue_front_end_assets();
@@ -122,13 +184,15 @@ class Test_Plugin extends \WP_UnitTestCase {
 	/**
 	 * Filter to test 'material_theme_builder_plugin_config'.
 	 *
-	 * @see Plugin::init()
 	 * @param array       $config Plugin config.
 	 * @param Plugin_Base $plugin Plugin instance.
+	 *
 	 * @return array
+	 * @see Plugin::init()
 	 */
 	public function filter_config( $config, $plugin ) {
 		unset( $config, $plugin ); // Test should actually use these.
+
 		return [ 'foo' => 'bar' ];
 	}
 
