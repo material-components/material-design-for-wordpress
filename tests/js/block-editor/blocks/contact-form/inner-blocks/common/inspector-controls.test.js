ef6dd861208305918a2d23e68e8cf96f73104543
David Lonjon <david.lonjon@gmail.com>
2020-04-24 21:43:24 +0900

    Add JS unit tests for the Contact Form block

---
+++ tests/js/block-editor/blocks/contact-form/inner-blocks/common/inspector-controls.test.js
@@ -0,0 +1,39 @@
+/**
+ * External dependencies
+ */
+import '@testing-library/jest-dom/extend-expect';
+import { shallow } from 'enzyme';
+import { cloneDeep } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import InspectorControls from '../../../../../../../assets/src/block-editor/blocks/contact-form/inner-blocks/common/components/inspector-controls';
+
+/**
+ * Render the component in a shallow mode using enzyme.
+ *
+ * @param {Object} props - Component props.
+ * @return {Function} A functional component.
+ */
+const setupShallow = props => {
+	return shallow( <InspectorControls { ...props } /> );
+};
+
+const baseProps = {
+	attributes: {
+		id: 'test-id',
+		label: 'Test Label',
+	},
+};
+
+describe( 'InspectorControls', () => {
+	afterEach( () => {
+		jest.clearAllMocks();
+	} );
+
+	it( 'matches snapshot', () => {
+		const wrapper = setupShallow( baseProps );
+		expect( wrapper ).toMatchSnapshot();
+	} );
+} );
