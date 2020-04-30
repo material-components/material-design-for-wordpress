b7d0b48533a11c7dbcd89390d653010e9742c8ae
Dukagjin Surdulli <me@dukagj.in>
2020-04-30 14:05:24 -0400

    Change block description for List

--- assets/src/block-editor/blocks/list/index.js
+++ assets/src/block-editor/blocks/list/index.js
@@ -14,7 +14,10 @@ export const name = 'material/list';
 
 export const settings = {
 	title: __( 'List', 'material-theme-builder' ),
-	description: __( 'List stuff in a list', 'material-theme-builder' ),
+	description: __(
+		'Lists allow you to list out your information in a clear manner.',
+		'material-theme-builder'
+	),
 	category: 'material',
 	icon: <i className="material-icons">list</i>,
 	keywords: [
