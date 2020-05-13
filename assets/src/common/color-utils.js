feb483b593e49e754b026973483bf107aa2c6e85
Ravi Chandra <ravichandrach1@gmail.com>
2020-05-14 00:34:01 +0530

    Fix JS error

--- assets/src/common/color-utils.js
+++ assets/src/common/color-utils.js
@@ -483,14 +483,15 @@ const colorUtils = {
 		return accessibility;
 	},
 	hexToRgb: hex =>
-		hex
-			.replace(
-				/^#?([a-f\d])([a-f\d])([a-f\d])$/i,
-				( m, r, g, b ) => '#' + r + r + g + g + b + b
-			)
-			.substring( 1 )
-			.match( /.{2}/g )
-			.map( x => parseInt( x, 16 ) ),
+		!hex ? [] :
+			hex
+				.replace(
+					/^#?([a-f\d])([a-f\d])([a-f\d])$/i,
+					( m, r, g, b ) => '#' + r + r + g + g + b + b
+				)
+				.substring( 1 )
+				.match( /.{2}/g )
+				.map( x => parseInt( x, 16 ) ),
 };
 
 export default colorUtils;
