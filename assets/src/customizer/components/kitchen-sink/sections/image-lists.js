192cc46951ac9fe3fd4d8c624ca877badec9d08b
Mario Aguiar <me@marioaguiar.net>
2020-05-22 13:38:18 -0500

    Update margin

--- assets/src/customizer/components/kitchen-sink/sections/image-lists.js
+++ assets/src/customizer/components/kitchen-sink/sections/image-lists.js
@@ -4,7 +4,7 @@ import { __ } from '@wordpress/i18n';
 
 const ImageLists = ( { radius } ) => (
 	<div>
-		<h4 className="mdc-typography--headline4">
+		<h4 className="mdc-typography--headline4" style={ { margin: 0 } }>
 			{ __( 'Image List', 'material-theme-builder' ) }
 		</h4>
 		<p>
