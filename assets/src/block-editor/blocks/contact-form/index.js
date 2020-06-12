4f61a08ee9381ca07fcc192c96517ec3e858a62e
Ravi Chandra <ravichandrach1@gmail.com>
2020-06-12 13:08:28 +0530

    Update block titles

--- assets/src/block-editor/blocks/contact-form/index.js
+++ assets/src/block-editor/blocks/contact-form/index.js
@@ -12,7 +12,7 @@ import save from './save';
 export const name = 'material/contact-form';
 
 export const settings = {
-	title: __( 'Contact Form', 'material-theme-builder' ),
+	title: __( 'Contact Form (Material)', 'material-theme-builder' ),
 	description: __(
 		'A simple way to get feedback from folks visiting your site.',
 		'material-theme-builder'
