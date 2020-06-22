6124b8d7e6baf703e372a4a244a7e821b523d2a0
Ravi Chandra <ravichandrach1@gmail.com>
2020-06-22 17:51:05 +0530

    Rewrite list block

--- assets/src/block-editor/blocks/list/edit.js
+++ assets/src/block-editor/blocks/list/edit.js
@@ -2,67 +2,147 @@
  * External dependencies
  */
 import classNames from 'classnames';
-
-/**
- * Internal dependencies
- */
-import { LIST_STYLES } from './options';
-import ImageRadioControl from '../../components/image-radio-control';
-import genericAttributesSetter from '../../utils/generic-attributes-setter';
+import { debounce } from 'lodash';
 
 /**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { createContext } from '@wordpress/element';
-import { PanelBody, ToggleControl } from '@wordpress/components';
-import { InnerBlocks, InspectorControls } from '@wordpress/block-editor';
+import { useState } from '@wordpress/element';
+import { PanelBody } from '@wordpress/components';
+import { InspectorControls } from '@wordpress/block-editor';
 
-export const ListContext = createContext( {
-	style: null,
-	parentClientId: null,
-	leadingIconsEnabled: false,
-	trailingIconsEnabled: false,
-} );
+/**
+ * Internal dependencies
+ */
+import { LIST_STYLES, ICON_POSITIONS, ICON_SIZES } from './options';
+import ButtonGroup from '../../components/button-group';
+import IconPicker from '../../components/icon-picker';
+import ImageRadioControl from '../../components/image-radio-control';
+import ToolbarUrlInputPopover from '../../components/toolbar-url-input-popover';
+import genericAttributesSetter from '../../utils/generic-attributes-setter';
+import ListItem from './components/list-item';
 
 /**
  * Material list edit component.
  */
 const ListEdit = ( {
-	attributes: { style, leadingIconsEnabled, trailingIconsEnabled },
+	attributes: { style, iconPosition, iconSize, items },
 	className,
-	clientId,
 	setAttributes,
 } ) => {
 	const setter = genericAttributesSetter( setAttributes );
+	const isSecondaryEnabled = style === 'two-line';
+	const [ selected, setSelected ] = useState( {
+		index: 0,
+		isSecondary: false,
+	} );
+
+	const addItem = () => {
+		items.push( {
+			primaryText: '',
+			secondaryText: '',
+			icon: 'favorite',
+		} );
+	};
+
+	const setItem = debounce( ( index, newItem ) => {
+		const newItems = [ ...items ];
+		const item = newItems[ index ] || {};
+		newItems[ index ] = { ...item, ...newItem };
+
+		setAttributes( { items: newItems } );
+	}, 300 );
+
+	const deleteItem = ( index, text ) => {
+		if ( index === 0 ) {
+			return;
+		}
+
+		const newItems = [ ...items ];
+		newItems.splice( index, 1 );
+		const prevItem = newItems[ index - 1 ];
+		prevItem.primaryText = `${ prevItem.primaryText } ${ text }`;
+		setAttributes( { items: newItems } );
+		setSelected( { index: index - 1 } );
+	};
+
+	const onEnter = ( index, isSecondary = false ) => {
+		if ( index >= items.length ) {
+			addItem();
+		}
+
+		setSelected( { index, isSecondary } );
+	};
+
+	const onIconChange = debounce( icon => {
+		const newItems = [ ...items ];
+		newItems[ selected.index ].icon = icon.name;
+		setAttributes( { items: newItems } );
+	}, 300 );
+
+	const onURLChange = debounce( url => {
+		const newItems = [ ...items ];
+		newItems[ selected.index ].url = url;
+		setAttributes( { items: newItems } );
+	}, 300 );
+
+	const onNewTabChange = debounce( newTab => {
+		const newItems = [ ...items ];
+		newItems[ selected.index ].target = newTab ? '_blank' : '';
+		setAttributes( { items: newItems } );
+	}, 300 );
+
+	const getSelectedItem = () => {
+		return items[ selected.index ] || {};
+	};
 
 	return (
-		<ListContext.Provider
-			value={ {
-				style,
-				leadingIconsEnabled,
-				trailingIconsEnabled,
-				parentClientId: clientId,
-			} }
-		>
-			<div
+		<>
+			<ul
 				className={ classNames(
 					'mdc-list',
 					className ? className.replace( 'mdc-list--two-line', '' ) : '',
 					{
-						'mdc-list--two-line': style === 'two-line',
+						'mdc-list--two-line': isSecondaryEnabled,
+						'mdc-list--avatar-list': 'large' === iconSize,
 					}
 				) }
 			>
-				<InnerBlocks
-					template={ [ [ 'material/list-item' ], [ 'material/list-item' ] ] }
-					allowedBlocks={ [ 'material/list-item' ] }
+				{ items.map( ( item, i ) => (
+					<ListItem
+						key={ i }
+						index={ i }
+						{ ...item }
+						{ ...{
+							iconPosition,
+							isSecondaryEnabled,
+						} }
+						onEnter={ onEnter }
+						onFocus={ ( index, isSecondary = false ) =>
+							setSelected( { index, isSecondary } )
+						}
+						isSelected={ i === selected.index }
+						isSecondarySelected={ i === selected.index && selected.isSecondary }
+						setItem={ setItem }
+						deleteItem={ deleteItem }
+					/>
+				) ) }
+			</ul>
+
+			{ selected && (
+				<ToolbarUrlInputPopover
+					url={ getSelectedItem().url }
+					setURL={ onURLChange }
+					isSelected={ true }
+					opensInNewTab={ getSelectedItem().target === '_blank' }
+					onChangeNewTab={ onNewTabChange }
 				/>
-			</div>
+			) }
 
 			<InspectorControls>
 				<PanelBody
-					title={ __( 'Style', 'material-theme-builder' ) }
+					title={ __( 'Block Settings', 'material-theme-builder' ) }
 					initialOpen={ true }
 				>
 					<ImageRadioControl
@@ -70,19 +150,37 @@ const ListEdit = ( {
 						options={ LIST_STYLES }
 						onChange={ setter( 'style' ) }
 					/>
-					<ToggleControl
-						label={ __( 'Leading Icons', 'material-theme-builder' ) }
-						checked={ leadingIconsEnabled }
-						onChange={ setter( 'leadingIconsEnabled' ) }
+					<ButtonGroup
+						label={ __( 'Icon Position' ) }
+						buttons={ ICON_POSITIONS }
+						current={ iconPosition }
+						onClick={ setter( 'iconPosition' ) }
 					/>
-					<ToggleControl
-						label={ __( 'Trailing Icons', 'material-theme-builder' ) }
-						checked={ trailingIconsEnabled }
-						onChange={ setter( 'trailingIconsEnabled' ) }
-					/>
+					{ 'leading' === iconPosition && (
+						<ButtonGroup
+							label={ __( 'Icon Size' ) }
+							buttons={ ICON_SIZES }
+							current={ iconSize }
+							onClick={ setter( 'iconSize' ) }
+						/>
+					) }
 				</PanelBody>
+
+				{ selected && 'none' !== iconPosition && (
+					<PanelBody
+						title={ __( 'List item Settings', 'material-theme-builder' ) }
+						initialOpen={ true }
+					>
+						<IconPicker
+							currentIcon={ {
+								name: getSelectedItem().icon,
+							} }
+							onChange={ onIconChange }
+						/>
+					</PanelBody>
+				) }
 			</InspectorControls>
-		</ListContext.Provider>
+		</>
 	);
 };
 
