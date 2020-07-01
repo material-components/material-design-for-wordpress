8cecb9bb245d27699b9cacdf1ecc7253696cd443
Mario Aguiar <me@marioaguiar.net>
2020-07-01 12:01:30 -0500

    Add notice component

---
+++ assets/src/wizard/components/notice/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * Prepare for notices
+ * @param {*} props Component props
+ */
+const Notice = props => {
+	return (
+		<div className={ `notice material-wizard__notice ${ props.type }` }>
+			<p>{ props.message }</p>
+		</div>
+	);
+};
+
+export default Notice;
