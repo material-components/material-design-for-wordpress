55545058733733c9e50bd58294061be7658b5c77
Mario Aguiar <me@marioaguiar.net>
2020-07-06 09:36:39 -0500

    Add basic examples

---
+++ assets/src/block-editor/blocks/cards-collection/example.js
@@ -0,0 +1,15 @@
+import { example as cardExample } from '../card/example';
+import { DEFAULT_NUMBER_OF_CARDS } from './constants';
+
+const cardsProps = [];
+
+for ( let index = 0; index < DEFAULT_NUMBER_OF_CARDS; index++ ) {
+	cardsProps.push( cardExample.attributes );
+}
+
+export const example = {
+	attributes: {
+		displayActions: false,
+		cardsProps,
+	},
+};
