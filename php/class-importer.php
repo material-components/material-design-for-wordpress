97bdbaa1569d96599b786c4915016f3aa7a2dd51
Mario Aguiar <me@marioaguiar.net>
2020-07-13 11:53:12 -0500

    Update webpack's config to add new module

--- php/class-importer.php
+++ php/class-importer.php
@@ -114,6 +114,44 @@ class Importer extends Module_Base {
 	}
 
 	/**
+	 * Enqueue getting started assets
+	 *
+	 * @action admin_enqueue_scripts
+	 *
+	 * @return void
+	 */
+	public function enqueue_assets() {
+		$screen = get_current_screen();
+
+		if ( 'material_page_material_settings' !== $screen->id ) {
+			return;
+		}
+
+		// Get Roboto Mono and icons.
+		wp_enqueue_style(
+			'google-fonts',
+			'//fonts.googleapis.com/css2?family=Roboto+Mono&family=Material+Icons',
+			[],
+			$this->plugin->asset_version()
+		);
+
+		wp_enqueue_style(
+			'material-gsm',
+			$this->plugin->asset_url( 'assets/css/wizard-compiled.css' ),
+			[ 'google-fonts' ],
+			$this->plugin->asset_version()
+		);
+
+		wp_enqueue_script(
+			'material-gsm',
+			$this->plugin->asset_url( 'assets/js/getting-started.js' ),
+			[ 'wp-element', 'wp-i18n' ],
+			$this->plugin->asset_version(),
+			true
+		);
+	}
+
+	/**
 	 * Import content after nonce verification
 	 *
 	 * @return string Status message
