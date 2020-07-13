430e689f9674b6cc2997f98c498c8025c8c3f171
Mario Aguiar <me@marioaguiar.net>
2020-07-13 12:40:07 -0500

    Add basic component structure

---
+++ assets/src/getting-started/components/content/index.js
@@ -0,0 +1,15 @@
+const Content = () => {
+	return (
+		<div className="material-gsm__content mdc-layout-grid__cell mdc-layout-grid__cell--span-9">
+			<h2 className="material-gsm__content-title">Onboarding Wizard</h2>
+			<p className="material-gsm__content-description">
+				Curabitur mauris erat congue posuere lacus torquent et convallis,
+				parturient rutrum cras senectus augue penatibus malesuada rhoncus,
+				ultricies finibus pretium arcu sagittis aliquet nisi.
+			</p>
+			<div className="material-gsm__content-actions"></div>
+		</div>
+	);
+};
+
+export default Content;
--- assets/src/getting-started/index.js
+++ assets/src/getting-started/index.js
@@ -1,1 +1,16 @@
-// Shh.
+import { render } from '@wordpress/element';
+import Navigation from './components/navigation';
+import Content from './components/content';
+
+const GettingStarted = () => {
+	return (
+		<div className="mdc-layout-grid">
+			<div className="mdc-layout-grid__inner">
+				<Navigation />
+				<Content />
+			</div>
+		</div>
+	);
+};
+
+render( <GettingStarted />, document.getElementById( 'material-gsm' ) );
