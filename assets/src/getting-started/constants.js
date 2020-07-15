8ad2f96732686e21f4248b43513077f443f779b8
Mario Aguiar <me@marioaguiar.net>
2020-07-15 11:30:14 -0500

    Update content based on active tab

--- assets/src/getting-started/constants.js
+++ assets/src/getting-started/constants.js
@@ -1,11 +1,36 @@
 import { __ } from '@wordpress/i18n';
 
 export const TABS = {
-	WIZARD: __( 'Onboarding wizard', 'material-theme-builder' ),
-	THEME: __( 'Material theme', 'material-theme-builder' ),
-	DEMO: __( 'Demo content', 'material-theme-builder' ),
-	EDITOR: __( 'Material Theme Editor', 'material-theme-builder' ),
-	BLOCKS: __( 'Material blocks', 'material-theme-builder' ),
+	WIZARD: {
+		title: __( 'Onboarding wizard', 'material-theme-builder' ),
+		action: __( 'Run Wizard', 'material-theme-builder' ),
+		content:
+			'Aenean egestas, ante vitae placerat tempor, felis ipsum finibus lectus, at eleifend neque tellus ac elit. Praesent cursus lectus felis, a ornare metus varius in.',
+	},
+	THEME: {
+		title: __( 'Material theme', 'material-theme-builder' ),
+		action: __( 'Install', 'material-theme-builder' ),
+		content:
+			'Aenean egestas, ante vitae placerat tempor, felis ipsum finibus lectus, at eleifend neque tellus ac elit. Praesent cursus lectus felis, a ornare metus varius in.',
+	},
+	DEMO: {
+		title: __( 'Demo content', 'material-theme-builder' ),
+		action: __( 'Install', 'material-theme-builder' ),
+		content:
+			'Aenean egestas, ante vitae placerat tempor, felis ipsum finibus lectus, at eleifend neque tellus ac elit. Praesent cursus lectus felis, a ornare metus varius in.',
+	},
+	EDITOR: {
+		title: __( 'Material Theme Editor', 'material-theme-builder' ),
+		action: __( 'Explore', 'material-theme-builder' ),
+		content:
+			'Aenean egestas, ante vitae placerat tempor, felis ipsum finibus lectus, at eleifend neque tellus ac elit. Praesent cursus lectus felis, a ornare metus varius in.',
+	},
+	BLOCKS: {
+		title: __( 'Material blocks', 'material-theme-builder' ),
+		action: __( 'Explore', 'material-theme-builder' ),
+		content:
+			'Aenean egestas, ante vitae placerat tempor, felis ipsum finibus lectus, at eleifend neque tellus ac elit. Praesent cursus lectus felis, a ornare metus varius in.',
+	},
 };
 
 export const ACTIONS = {
