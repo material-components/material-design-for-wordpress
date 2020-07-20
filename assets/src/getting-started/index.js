90fcce3b04ce04b6022fdbc921f7182f3e251670
Mario Aguiar <me@marioaguiar.net>
2020-07-20 09:39:31 -0500

    Add tests for reducer

--- assets/src/getting-started/index.js
+++ assets/src/getting-started/index.js
@@ -1,19 +1,4 @@
 import { render } from '@wordpress/element';
-import { TabProvider } from './context';
-import Navigation from './components/navigation';
-import Content from './components/content';
-
-const GettingStarted = () => {
-	return (
-		<TabProvider>
-			<div className="mdc-layout-grid mdc-typography">
-				<div className="mdc-layout-grid__inner">
-					<Navigation />
-					<Content />
-				</div>
-			</div>
-		</TabProvider>
-	);
-};
+import GettingStarted from './components';
 
 render( <GettingStarted />, document.getElementById( 'material-gsm' ) );
