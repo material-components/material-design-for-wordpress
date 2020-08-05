4f43601bfd8e79611bcac38b2c263857eb3cd225
rodydavis <rody.davis.jr@gmail.com>
2020-08-05 16:15:14 -0700

    updating format

--- assets/src/getting-started/constants.js
+++ assets/src/getting-started/constants.js
@@ -1,26 +1,26 @@
 import { __ } from '@wordpress/i18n';
 
 export const ACTIONS = {
-    NEXT_STEP: 'NEXT_STEP',
-    GOTO_STEP: 'GOTO_STEP',
-    MARK_COMPLETE: 'MARK_COMPLETE',
-    ERROR: 'ERROR',
-    SET_THEME_OK: 'SET_THEME_OK',
-    SET_DEMO_OK: 'SET_DEMO_OK',
-    INSTALL_THEME: 'INSTALL_THEME',
-    ACTIVATE_THEME: 'ACTIVATE_THEME',
-    INSTALL_DEMO_CONTENT: 'INSTALL_DEMO_CONTENT',
-    THEME_INSTALLED: 'THEME_INSTALLED',
-    DEMO_INSTALLED: 'DEMO_INSTALLED',
+	NEXT_STEP: 'NEXT_STEP',
+	GOTO_STEP: 'GOTO_STEP',
+	MARK_COMPLETE: 'MARK_COMPLETE',
+	ERROR: 'ERROR',
+	SET_THEME_OK: 'SET_THEME_OK',
+	SET_DEMO_OK: 'SET_DEMO_OK',
+	INSTALL_THEME: 'INSTALL_THEME',
+	ACTIVATE_THEME: 'ACTIVATE_THEME',
+	INSTALL_DEMO_CONTENT: 'INSTALL_DEMO_CONTENT',
+	THEME_INSTALLED: 'THEME_INSTALLED',
+	DEMO_INSTALLED: 'DEMO_INSTALLED',
 };
 
 /**
  * @todo Probably better if we turn these into components like the wizard
  */
 export const TABS = {
-    WIZARD: __('Onboarding wizard', 'material-theme-builder'),
-    THEME: __('Material theme', 'material-theme-builder'),
-    DEMO: __('Demo content', 'material-theme-builder'),
-    EDITOR: __('Material Theme Options', 'material-theme-builder'),
-    BLOCKS: __('Material blocks', 'material-theme-builder'),
+	WIZARD: __( 'Onboarding wizard', 'material-theme-builder' ),
+	THEME: __( 'Material theme', 'material-theme-builder' ),
+	DEMO: __( 'Demo content', 'material-theme-builder' ),
+	EDITOR: __( 'Material Theme Options', 'material-theme-builder' ),
+	BLOCKS: __( 'Material blocks', 'material-theme-builder' ),
 };
