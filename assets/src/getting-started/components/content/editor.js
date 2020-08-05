694610c91725f59e5186405e0184524e019b2b09
rodydavis <rody.davis.jr@gmail.com>
2020-08-05 16:06:06 -0700

    Material Theme Options

--- assets/src/getting-started/components/content/editor.js
+++ assets/src/getting-started/components/content/editor.js
@@ -3,24 +3,23 @@ import { __ } from '@wordpress/i18n';
 import Button from '../../../wizard/components/navigation/button';
 
 export const Editor = props => {
-	return (
-		<Fragment>
-			<h2 className="material-gsm__content-title mdc-typography--headline6">
-				{ __( 'Material Theme Customizer', 'material-theme-builder' ) }
-			</h2>
-			<p className="material-gsm__content-description">
-				Aenean egestas, ante vitae placerat tempor, felis ipsum finibus lectus,
-				at eleifend neque tellus ac elit. Praesent cursus lectus felis, a ornare
-				metus varius in.
-			</p>
-			<div className="material-gsm__content-actions">
-				<Button
-					style="mdc-button--raised"
-					text={ __( 'Explore', 'material-theme-builder' ) }
-					trailingIcon="navigate_next"
-					onClick={ props.handleClick }
-				/>
-			</div>
-		</Fragment>
-	);
+    return ( <
+        Fragment >
+        <
+        h2 className = "material-gsm__content-title mdc-typography--headline6" > { __('Material Theme Options', 'material-theme-builder') } <
+        /h2> <
+        p className = "material-gsm__content-description" >
+        Aenean egestas, ante vitae placerat tempor, felis ipsum finibus lectus,
+        at eleifend neque tellus ac elit.Praesent cursus lectus felis, a ornare metus varius in . <
+        /p> <
+        div className = "material-gsm__content-actions" >
+        <
+        Button style = "mdc-button--raised"
+        text = { __('Explore', 'material-theme-builder') }
+        trailingIcon = "navigate_next"
+        onClick = { props.handleClick }
+        /> <
+        /div> <
+        /Fragment>
+    );
 };
