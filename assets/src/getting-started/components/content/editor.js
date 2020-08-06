1e846d8794e554a740878c0282c1b84c1cc5d97c
rodydavis <rody.davis.jr@gmail.com>
2020-08-05 17:19:16 -0700

    fixing all tests locally

--- assets/src/getting-started/components/content/editor.js
+++ assets/src/getting-started/components/content/editor.js
@@ -6,22 +6,22 @@ export const Editor = props => {
 	return (
 		<Fragment>
 			<h2 className="material-gsm__content-title mdc-typography--headline6">
-				{ ' ' }
-				{ __( 'Material Theme Options', 'material-theme-builder' ) }{ ' ' }
-			</h2>{ ' ' }
+				
+				{ __( 'Material Theme Options', 'material-theme-builder' ) }
+			</h2>
 			<p className="material-gsm__content-description">
 				Aenean egestas, ante vitae placerat tempor, felis ipsum finibus lectus,
-				at eleifend neque tellus ac elit.Praesent cursus lectus felis, a ornare
-				metus varius in .{ ' ' }
-			</p>{ ' ' }
+				at eleifend neque tellus ac elit. Praesent cursus lectus felis, a ornare
+				metus varius in.
+			</p>
 			<div className="material-gsm__content-actions">
 				<Button
 					style="mdc-button--raised"
 					text={ __( 'Explore', 'material-theme-builder' ) }
 					trailingIcon="navigate_next"
 					onClick={ props.handleClick }
-				/>{ ' ' }
-			</div>{ ' ' }
+				/>
+			</div>
 		</Fragment>
 	);
 };
