5d22390a33e102bf05963f00640b212eafbeb15d
Ravi Chandra <ravichandrach1@gmail.com>
2020-08-06 17:28:30 +0530

    Add reset button for global shape size control

--- assets/src/block-editor/components/global-shape-size/index.js
+++ assets/src/block-editor/components/global-shape-size/index.js
@@ -7,14 +7,46 @@ import { RangeControl } from '@wordpress/components';
  * Internal dependencies
  */
 import { withGlobalDefault } from '../with-global-default';
+import { __ } from '@wordpress/i18n';
+import './style.css';
 
+/**
+ * Global shape size control, if no value is set,
+ * gets the shapesize value from customizer.
+ *
+ * @param {Object} props
+ */
+const GlobalShapeSize = props => {
+	const onReset = event => {
+		event.preventDefault();
+		if ( props.onChange ) {
+			props.onChange( undefined );
+		}
+	};
+
+	return (
+		<div className="components-global-shape-size">
+			<RangeControl { ...props } />
+			<button
+				type="button"
+				className="components-button global-shape-size-reset is-small"
+				onClick={ onReset }
+			>
+				{ __( 'Reset', 'material-theme-builder' ) }
+			</button>
+		</div>
+	);
+};
+
+/**
+ * Helper HOC to add a `attributeName` prop to a component.
+ *
+ * @param {Function} WrappedComponent Component to be wrapped.
+ * @param {string} attributeName Attribute name.
+ */
 const withAttributeName = ( WrappedComponent, attributeName ) => props => (
 	<WrappedComponent { ...props } attributeName={ attributeName } />
 );
-
-const GlobalShapeSize = props => {
-	return <RangeControl { ...props } />;
-};
 
 export default withAttributeName(
 	withGlobalDefault( GlobalShapeSize ),
