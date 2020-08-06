5743bb862fbe8e4296ef45460c0ebd1a34261fa8
Mario Aguiar <me@marioaguiar.net>
2020-08-06 09:47:21 -0500

    Transform buttons to material buttons

---
+++ assets/src/block-editor/blocks/button/transforms.js
@@ -0,0 +1,19 @@
+import { createBlock } from '@wordpress/blocks';
+
+const transforms = {
+	from: [
+		{
+			type: 'block',
+			blocks: [ 'core/buttons' ],
+			transform: ( buttonsAttributes, buttons ) => {
+				return buttons.map( ( { attributes } ) => {
+					return createBlock( 'material/button', {
+						label: attributes.text,
+					} );
+				} );
+			},
+		},
+	],
+};
+
+export default transforms;
