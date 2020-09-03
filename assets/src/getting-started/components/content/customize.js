583e30f8e069987e44e0fad99d5e9d19525d3c08
Rody Davis <31253215+rodydavis@users.noreply.github.com>
2020-09-03 15:12:13 -0700

    updating meta text with links

--- assets/src/getting-started/components/content/customize.js
+++ assets/src/getting-started/components/content/customize.js
@@ -26,50 +26,57 @@ export const Customize = props => {
 				{ __( 'Starter Styles', 'material-theme-builder' ) }
 			</h2>
 			<p className="material-gsm__content-description">
-				Start from our existing styles and take advantage of{ ' ' }
+				{ __(
+					'Start from our existing styles and take advantage of ',
+					'material-theme-builder'
+				) }
 				<a
 					href="https://material.io/design/material-theming/overview.html"
 					target="_blank"
 				>
-					Material Theming
-				</a>{ ' ' }
-				by customizing your theme to truly make your site your own.
+					{ __( 'Material Theming', 'material-theme-builder' ) }
+				</a>
+				{ __(
+					' by customizing your theme to truly make your site your own.',
+					'material-theme-builder'
+				) }
 			</p>
 
 			<h2 className="material-gsm__content-title mdc-typography--headline6">
 				{ __( 'Color Palettes', 'material-theme-builder' ) }
 			</h2>
 			<p className="material-gsm__content-description">
-				Change your primary and secondary colors and see your site come to life
-				with Material Design’s color system. Need some help? Try Material’s
+				{ __(
+					'Change your primary and secondary colors and see your site come to life with Material Design’s color system. Need some help? Try Material’s ',
+					'material-theme-builder'
+				) }
 				<a
 					href="https://material.io/design/color/the-color-system.html#tools-for-picking-colors"
 					target="_blank"
 				>
-					{ ' ' }
-					color palette generator
-				</a>{ ' ' }
-				to find complementary, analogous, and triadic tonal palettes.
+					{ __( 'color palette generator', 'material-theme-builder' ) }
+				</a>
 			</p>
 
 			<h2 className="material-gsm__content-title mdc-typography--headline6">
 				{ __( 'Typography (Font Styles)', 'material-theme-builder' ) }
 			</h2>
 			<p className="material-gsm__content-description">
-				Choose from the full set of{ ' ' }
+				{ __( 'Choose from the full set of ', 'material-theme-builder' ) }
 				<a href="https://fonts.google.com/" target="_blank">
-					Google Fonts
+					{ __( 'Google Fonts.', 'material-theme-builder' ) }
 				</a>
-				. Headlines are a great place to inject expression. To retain
-				legibility, utilitarian fonts are better for smaller text sizes. Read
-				Material’s{ ' ' }
+				{ __(
+					' Headlines are a great place to inject expression. To retain legibility, utilitarian fonts are better for smaller text sizes. Read Material’s ',
+					'material-theme-builder'
+				) }
 				<a
 					href="https://material.io/design/typography/the-type-system.html"
 					target="_blank"
 				>
-					type system
-				</a>{ ' ' }
-				for more guidance.
+					{ __( 'type system', 'material-theme-builder' ) }
+				</a>
+				{ __( '  for more guidance.', 'material-theme-builder' ) }
 			</p>
 
 			<h2 className="material-gsm__content-title mdc-typography--headline6">
@@ -86,13 +93,14 @@ export const Customize = props => {
 				{ __( 'Icon Style', 'material-theme-builder' ) }
 			</h2>
 			<p className="material-gsm__content-description">
-				All of the{ ' ' }
+				{ __( 'All of the ', 'material-theme-builder' ) }
 				<a href="http://material.io/icons" target="_blank">
-					Material Icons
-				</a>{ ' ' }
-				and their variants are included with this plugin. Choose from Filled,
-				Outlined, Rounded, Two-tone, or Sharp icons that best reflect your
-				style.
+					{ __( 'Material Icons', 'material-theme-builder' ) }
+				</a>
+				{ __(
+					' and their variants are included with this plugin. Choose from Filled, Outlined, Rounded, Two-tone, or Sharp icons that best reflect your style.',
+					'material-theme-builder'
+				) }
 			</p>
 
 			<div className="material-gsm__content-actions">
