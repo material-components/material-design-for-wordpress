2dfa4051e2f1c469e0eab9b7b86830d8ae6136e2
Ravi Chandra <ravichandrach1@gmail.com>
2020-12-23 01:09:27 +0530

    Fix rich-text compat issues with focus

--- assets/src/block-editor/components/polyfills/index.js
+++ assets/src/block-editor/components/polyfills/index.js
@@ -1,2 +1,5 @@
-export { default as RichText } from './rich-text';
+export {
+	default as RichText,
+	isExperimental as isExperimentalRichText,
+} from './rich-text';
 export { default as ToolbarGroup } from './toolbar-group';
