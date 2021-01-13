906f1f23390870e40da96c8d1fd9fb7f5da3db3f
Ravi Chandra <ravichandrach1@gmail.com>
2021-01-13 23:56:44 +0530

    Remove RichText component polyfill

--- assets/src/block-editor/components/polyfills/rich-text.js
+++
@@ -1,6 +0,0 @@
-import { __experimentalRichText } from '@wordpress/rich-text';
-import { RichText } from '@wordpress/block-editor';
-
-export default __experimentalRichText || RichText;
-export const isExperimental = 'undefined' !== typeof __experimentalRichText;
-console.log( isExperimental );
