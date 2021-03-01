75e33672abc38763d86dbd9f4fdcf46fa30293b2
Mario Aguiar <me@marioaguiar.net>
2021-03-01 14:19:05 -0600

    Setup context

--- assets/src/settings/components/index.js
+++ assets/src/settings/components/index.js
@@ -6,11 +6,12 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
+import { SettingsProvider } from '../context';
 import Integrations from './integrations';
 
 const Settings = () => {
 	return (
-		<>
+		<SettingsProvider>
 			<div className="mdc-layout-grid__inner">
 				<h1 className="mdc-typography--headline3">
 					{ __( 'Material Settings', 'material-design' ) }
@@ -18,7 +19,7 @@ const Settings = () => {
 
 				<Integrations />
 			</div>
-		</>
+		</SettingsProvider>
 	);
 };
 
---
+++ assets/src/settings/reducer.js
@@ -0,0 +1,9 @@
+/**
+ * Actions to be taken during the app's life circle
+ *
+ * @param {*} state  Current state of the world
+ * @param {*} action Action dispatched
+ */
+export const reducer = state => {
+	return state;
+};
