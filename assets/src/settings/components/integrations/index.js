c0cfc226de35a072d355573a97eca134d1d44044
Mario Aguiar <me@marioaguiar.net>
2021-03-01 16:40:35 -0600

    Add links to instructions

--- assets/src/settings/components/integrations/index.js
+++ assets/src/settings/components/integrations/index.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 
 const Integrations = () => {
 	return (
@@ -10,12 +10,25 @@ const Integrations = () => {
 				{ __( 'Integrations', 'material-design' ) }
 			</h2>
 
-			<p className="mdc-typography--body1">
-				{ __(
-					'Integrate Google Fonts and Material icons to get the most out of the Material Theme.',
-					'material-design'
-				) }
-			</p>
+			<p
+				className="mdc-typography--body1"
+				dangerouslySetInnerHTML={ {
+					__html: sprintf(
+						__(
+							'Integrate %s and %s to get the most out of the Material Theme.',
+							'material-design'
+						),
+						`<a href="http://google.com">${ __(
+							'Google Fonts',
+							'material-design'
+						) }</a>`,
+						`<a href="http://material.io">${ __(
+							'Material icons',
+							'material-design'
+						) }</a>`
+					),
+				} }
+			></p>
 
 			<p className="mdc-typography--body1">
 				{ __(
