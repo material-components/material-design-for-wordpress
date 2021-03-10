f5f94d67f5ead6b92ff2baf2dfa531863b21b293
Mario Aguiar <me@marioaguiar.net>
2021-03-10 16:34:30 -0600

    Add remove button

--- assets/src/settings/components/integrations/api.js
+++ assets/src/settings/components/integrations/api.js
@@ -13,29 +13,56 @@ import SettingsContext from '../../context';
 const Api = () => {
 	const { state } = useContext( SettingsContext );
 	const [ api, setApi ] = useState( null );
+	const isApiOk = 'ok' === state.apiStatus;
 
 	return (
 		<div className="material-settings__api mdc-layout-grid">
 			<div className="mdc-layout-grid__inner">
-				<TextControl
-					className="material-settings__api-input mdc-layout-grid__cell"
-					value={ api }
-					placeholder={ __( 'Google API Key', 'material-design' ) }
-					onChange={ value => {
-						console.log( value );
-						setApi( value );
-					} }
-				/>
+				{ ! isApiOk && (
+					<TextControl
+						className="material-settings__api-input mdc-layout-grid__cell"
+						value={ api }
+						placeholder={ __( 'Google API Key', 'material-design' ) }
+						onChange={ value => {
+							console.log( value );
+							setApi( value );
+						} }
+					/>
+				) }
+
+				{ isApiOk && (
+					<TextControl
+						className="material-settings__api-input material-settings__api-input--disabled mdc-layout-grid__cell"
+						value="•••••••••••••••••••••••••••••"
+						disabled
+					/>
+				) }
 
 				<div className="mdc-layout-grid__cell">
-					<button
-						className="mdc-button mdc-button--raised"
-						disabled={ ! api && ! state.api }
-					>
-						<span className="mdc-button__label">
-							{ __( 'Activate', 'material-design' ) }
-						</span>
-					</button>
+					{ ! isApiOk && (
+						<button
+							className="mdc-button mdc-button--raised"
+							disabled={ ! api }
+						>
+							<span className="mdc-button__label">
+								{ __( 'Activate', 'material-design' ) }
+							</span>
+						</button>
+					) }
+
+					{ isApiOk && (
+						<button className="mdc-button">
+							<i
+								className="material-icons mdc-button__icon leading-icon"
+								aria-hidden="true"
+							>
+								delete
+							</i>
+							<span className="mdc-button__label">
+								{ __( 'Remove', 'material-design' ) }
+							</span>
+						</button>
+					) }
 				</div>
 			</div>
 		</div>
