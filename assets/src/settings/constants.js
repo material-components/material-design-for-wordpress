543987748e07cda7eda25d937a394da4c73095ec
Mario Aguiar <me@marioaguiar.net>
2021-03-11 09:58:02 -0600

    Allow removal of api key

--- assets/src/settings/constants.js
+++ assets/src/settings/constants.js
@@ -2,9 +2,12 @@ export const ACTIONS = {
 	ERROR: 'ERROR',
 	TOGGLE_UPDATES: 'TOGGLE_UPDATES',
 	REMOVE_API_KEY: 'REMOVE_API_KEY',
+	ADD_API_KEY: 'ADD_API_KEY',
 };
 
 export const UPDATERS = {
 	FONT: 'FONT',
 	ICON: 'ICON',
 };
+
+export const KEY_PLACEHOLDER = '•••••••••••••••••••••••••••••';
