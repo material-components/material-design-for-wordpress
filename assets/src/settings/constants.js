db9764a29ff89d32385a4fde2d05293f3d2ad182
Mario Aguiar <me@marioaguiar.net>
2021-04-01 14:30:30 -0600

    Retrieve available updates from backend

--- assets/src/settings/constants.js
+++ assets/src/settings/constants.js
@@ -22,7 +22,7 @@ export const UPDATERS = {
 		type: 'FONTS',
 		lastUpdated: parseInt( getConfig( 'fontsLastUpdated' ), 10 ),
 		needsKey: true,
-		updateAvailable: false,
+		updateAvailable: 'update' === getConfig( 'fontsUpdateStatus' ),
 		autoUpdates: parseInt( getConfig( 'fontsAutoUpdate' ), 10 ),
 	},
 	ICONS: {
@@ -30,7 +30,7 @@ export const UPDATERS = {
 		type: 'ICONS',
 		lastUpdated: parseInt( getConfig( 'iconsLastUpdated' ), 10 ),
 		needsKey: false,
-		updateAvailable: true,
+		updateAvailable: 'update' === getConfig( 'iconsUpdateStatus' ),
 		autoUpdates: parseInt( getConfig( 'iconsAutoUpdate' ), 10 ),
 	},
 };
