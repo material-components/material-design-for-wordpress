67a5763e16ea006ec5f506b1d5a9b318d0b4340f
Mario Aguiar <mario.aguiar@xwp.co>
2021-04-07 13:17:42 +0530

    Add settings page

---
+++ plugin/assets/src/settings/constants.js
@@ -0,0 +1,38 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import getConfig from '../admin/get-config';
+
+export const ACTIONS = {
+	ERROR: 'ERROR',
+	TOGGLE_UPDATES: 'TOGGLE_UPDATES',
+	REMOVE_API_KEY: 'REMOVE_API_KEY',
+	ADD_API_KEY: 'ADD_API_KEY',
+	SET_UPDATED: 'SET_UPDATED',
+};
+
+export const UPDATERS = {
+	FONTS: {
+		title: __( 'Google Fonts', 'material-design' ),
+		type: 'FONTS',
+		lastUpdated: parseInt( getConfig( 'fontsLastUpdated' ), 10 ),
+		needsKey: true,
+		updateAvailable: false,
+		autoUpdates: parseInt( getConfig( 'fontsAutoUpdate' ), 10 ),
+	},
+	ICONS: {
+		title: __( 'Material Icons', 'material-design' ),
+		type: 'ICONS',
+		lastUpdated: parseInt( getConfig( 'iconsLastUpdated' ), 10 ),
+		needsKey: false,
+		updateAvailable: true,
+		autoUpdates: parseInt( getConfig( 'iconsAutoUpdate' ), 10 ),
+	},
+};
+
+export const KEY_PLACEHOLDER = '•••••••••••••••••••••••••••••';
---
+++ plugin/assets/src/settings/context.js
@@ -0,0 +1,39 @@
+/**
+ * WordPress dependencies
+ */
+import { useReducer, createContext } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { STATUS } from '../wizard/constants';
+import { UPDATERS } from './constants';
+import { reducer } from './reducer';
+import getConfig from '../admin/get-config';
+
+/**
+ * Setups context to be used across the app
+ */
+const SettingsContext = createContext();
+
+const { Provider } = SettingsContext;
+
+const initialState = {
+	status: STATUS.IDLE,
+	apiStatus: getConfig( 'apiStatus' ),
+	updaters: UPDATERS,
+	error: {},
+};
+
+/**
+ * Creates a wrapper in order to use our custom reducer
+ *
+ * @param {*} param Children to render
+ */
+export const SettingsProvider = ( { children } ) => {
+	const [ state, dispatch ] = useReducer( reducer, initialState );
+
+	return <Provider value={ { state, dispatch } }>{ children }</Provider>;
+};
+
+export default SettingsContext;
