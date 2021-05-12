61d5fac2e8033eb7f1d65f8c20250b1b35d27fe1
Utkarsh Patel <itismeutkarsh@gmail.com>
2021-05-12 21:10:25 +0530

    Add customizer settings

--- theme/functions.php
+++ theme/functions.php
@@ -220,6 +220,7 @@ require get_template_directory() . '/inc/customizer/colors.php';
 require get_template_directory() . '/inc/customizer/header-footer.php';
 require get_template_directory() . '/inc/customizer/layout.php';
 require get_template_directory() . '/inc/customizer/menu.php';
+require get_template_directory() . '/inc/customizer/global-style.php';
 
 /**
  * Custom menu walker
@@ -259,4 +260,5 @@ MaterialDesign\Theme\Customizer\Colors\setup();
 MaterialDesign\Theme\Customizer\Header_Footer\setup();
 MaterialDesign\Theme\Customizer\Layout\setup();
 MaterialDesign\Theme\Customizer\Menu\setup();
+MaterialDesign\Theme\Customizer\Global_Style\setup();
 MaterialDesign\Theme\Widgets\setup();
