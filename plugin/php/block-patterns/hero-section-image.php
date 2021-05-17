f12482998b6c41ee45b788c5772e843b82fa9282
Mario Aguiar <mario.aguiar@xwp.co>
2021-05-17 11:13:35 -0500

    Add image to pattern

--- plugin/php/block-patterns/hero-section-image.php
+++ plugin/php/block-patterns/hero-section-image.php
@@ -18,14 +18,18 @@
  */
 
 /**
- * Hero section pattern.
+ * Hero section + image pattern.
  *
  * @package MaterialDesign
  */
 
+use function MaterialDesign\Plugin\get_plugin_instance;
+
+$image = esc_url( get_plugin_instance()->asset_url( 'assets/images/placeholder.png' ) );
+
 return [
 	'title'         => __( 'Hero section + Image', 'material-design' ),
-	'content'       => '<!-- wp:group {"align":"wide"} --><div class="wp-block-group alignwide"><div class="wp-block-group__inner-container"><!-- wp:columns {"align":"wide"} --><div class="wp-block-columns alignwide"><!-- wp:column --><div class="wp-block-column"><!-- wp:heading --><h2>Landing Page Hero Section</h2><!-- /wp:heading --><!-- wp:paragraph --><p>Imperdiet taciti nostra pulvinar bibendum congue varius sapien, diam curae pellentesque quam auctor praesent nullam convallis, tincidunt urna suscipit litora netus dictum.</p><!-- /wp:paragraph --><!-- wp:material/buttons --><div class="wp-block-material-buttons"><!-- wp:material/button {"iconPosition":"none"} --><div class="wp-block-material-button" id="block-material-button-51"><button class="mdc-button mdc-button--raised"><div class="mdc-button__ripple"></div><span class="mdc-button__label">Get Started</span></button></div><!-- /wp:material/button --></div><!-- /wp:material/buttons --></div><!-- /wp:column --><!-- wp:column --><div class="wp-block-column"><!-- wp:image --><figure class="wp-block-image"><img alt=""/></figure><!-- /wp:image --></div><!-- /wp:column --></div><!-- /wp:columns --></div></div><!-- /wp:group -->',
+	'content'       => "<!-- wp:group {\"align\":\"wide\"} --><div class=\"wp-block-group alignwide\"><div class=\"wp-block-group__inner-container\"><!-- wp:columns {\"align\":\"wide\"} --><div class=\"wp-block-columns alignwide\"><!-- wp:column --><div class=\"wp-block-column\"><!-- wp:heading --><h2>Landing Page Hero Section</h2><!-- /wp:heading --><!-- wp:paragraph --><p>Imperdiet taciti nostra pulvinar bibendum congue varius sapien, diam curae pellentesque quam auctor praesent nullam convallis, tincidunt urna suscipit litora netus dictum.</p><!-- /wp:paragraph --><!-- wp:material/buttons --><div class=\"wp-block-material-buttons\"><!-- wp:material/button {\"iconPosition\":\"none\"} --><div class=\"wp-block-material-button\" id=\"block-material-button-51\"><button class=\"mdc-button mdc-button--raised\"><div class=\"mdc-button__ripple\"></div><span class=\"mdc-button__label\">Get Started</span></button></div><!-- /wp:material/button --></div><!-- /wp:material/buttons --></div><!-- /wp:column --><!-- wp:column --><div class=\"wp-block-column\"><!-- wp:image {\"id\":127,\"sizeSlug\":\"large\",\"linkDestination\":\"none\"} --><figure class=\"wp-block-image size-large\"><img src=\"$image\" alt=\"\" class=\"wp-image-127\"/></figure><!-- /wp:image --></div><!-- /wp:column --></div><!-- /wp:columns --></div></div><!-- /wp:group -->",
 	'viewportWidth' => 800,
 	'categories'    => [ 'material', 'buttons', 'header', 'image' ],
 	'description'   => __( 'Hero section with image', 'material-design' ),
