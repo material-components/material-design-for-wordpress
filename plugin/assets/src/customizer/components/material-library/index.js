0bfbd16d1f9c53ffdc8d3d4ee20cca171440ce45
pbearne <pbearne@gmail.com>
2021-07-22 12:16:31 -0400

    fixing coding standards

--- plugin/assets/src/customizer/components/material-library/index.js
+++ plugin/assets/src/customizer/components/material-library/index.js
@@ -103,8 +103,16 @@ const MaterialLibrary = ( {
 				onSecondaryColor={ onSecondaryColor }
 				surfaceColor={ surfaceColor }
 				onSurfaceColor={ onSurfaceColor }
-				surfaceColorMix4={ colorUtils.mix( surfaceColor, onSurfaceColor, 0.04 ) }
-				surfaceColorMix12={ colorUtils.mix( surfaceColor, onSurfaceColor, 0.12 ) }
+				surfaceColorMix4={ colorUtils.mix(
+					surfaceColor,
+					onSurfaceColor,
+					0.04
+				) }
+				surfaceColorMix12={ colorUtils.mix(
+					surfaceColor,
+					onSurfaceColor,
+					0.12
+				) }
 				backgroundColor={ backgroundColor }
 				onBackgroundColor={ onBackgroundColor }
 				iconCollection={ materialIconFontName( iconCollection ) }
