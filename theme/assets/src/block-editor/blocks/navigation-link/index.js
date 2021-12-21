69855a69035adb3c0c498755cb6217e676bfd5a1
Mario Aguiar <mario.aguiar@xwp.co>
2021-12-21 10:30:04 -0600

    Add block override

--- theme/assets/src/block-editor/blocks/navigation-link/index.js
+++ theme/assets/src/block-editor/blocks/navigation-link/index.js
@@ -20,8 +20,29 @@ const addMaterialStyle = ( settings, name ) => {
 	return settings;
 };
 
+const withNavigationLinkEdit = BlockEdit => {
+	return props => {
+		if ( 'core/navigation-link' === props.name ) {
+			console.log('modified block');
+			return (
+				<>
+					<BlockEdit { ...props } />
+				</>
+			);
+		}
+
+		return <BlockEdit { ...props } />;
+	};
+};
+
 addFilter(
 	'blocks.registerBlockType',
 	'material/navigation-link-style',
 	addMaterialStyle
+);
+
+addFilter(
+	'editor.BlockEdit',
+	'material/navigation-link-edit',
+	withNavigationLinkEdit,
 );
