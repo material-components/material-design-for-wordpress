3061097d14cf23b46f12aa09244d12a01c529f53
Utkarsh Patel <itismeutkarsh@gmail.com>
2021-12-22 15:24:52 +0530

    Remove unused save

--- theme/assets/src/block-editor/blocks/search/index.js
+++ theme/assets/src/block-editor/blocks/search/index.js
@@ -27,7 +27,6 @@ import '@material/form-field/dist/mdc.form-field.css';
 import metadata from './block.json';
 import { icon } from './icon';
 import edit from './edit';
-import save from './save';
 
 const { name, title } = metadata;
 
@@ -38,5 +37,4 @@ export const settings = {
 	description: metadata.description,
 	icon,
 	edit,
-	save,
 };
