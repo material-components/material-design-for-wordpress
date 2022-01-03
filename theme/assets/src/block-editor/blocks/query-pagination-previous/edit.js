3b3779b5b3c6621276762da044e497a052b84417
Mario Aguiar <mario.aguiar@xwp.co>
2022-01-03 13:10:03 -0600

    Add previous block

---
+++ theme/assets/src/block-editor/blocks/query-pagination-previous/edit.js
@@ -0,0 +1,29 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { useBlockProps } from '@wordpress/block-editor';
+
+const QueryPaginationPreviousEdit = ( {
+	attributes: { label },
+	setAttributes,
+	context: { paginationArrow },
+} ) => {
+	return (
+		<a
+			href="#pagination-previous-pseudo-link"
+			className="mdc-ripple-surface"
+			onClick={ event => event.preventDefault() }
+			{ ...useBlockProps() }
+		>
+			<span className="material-icons" aria-hidden="true">
+				chevron_left
+			</span>
+			<span className="screen-reader-text">
+				{ __( 'Previous page', 'material-design-google' ) }
+			</span>
+		</a>
+	);
+};
+
+export default QueryPaginationPreviousEdit;
---
+++ theme/assets/src/block-editor/blocks/query-pagination-previous/index.js
@@ -0,0 +1,17 @@
+/**
+ * Internal dependencies
+ */
+import metadata from './block.json';
+import edit from './edit';
+
+const { name } = metadata;
+export { metadata, name };
+
+export const settings = {
+	icon: () => (
+		<span className="material-icons material-icons__button">
+			navigate_before
+		</span>
+	),
+	edit,
+};
