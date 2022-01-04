b79db2f08f06390a4318c66a85766f7720278be5
Mario Aguiar <mario.aguiar@xwp.co>
2022-01-04 09:46:24 -0600

    Add last block

---
+++ theme/assets/src/block-editor/blocks/query-pagination-last/index.js
@@ -0,0 +1,15 @@
+/**
+ * Internal dependencies
+ */
+import metadata from './block.json';
+import edit from './edit';
+
+const { name } = metadata;
+export { metadata, name };
+
+export const settings = {
+	icon: () => (
+		<span className="material-icons material-icons__button">last_page</span>
+	),
+	edit,
+};
