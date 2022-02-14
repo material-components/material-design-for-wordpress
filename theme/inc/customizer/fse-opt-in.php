56da306cc5766f64a79c61da17d61282fbf9b3b3
Utkarsh Patel <itismeutkarsh@gmail.com>
2022-02-14 22:15:59 +0530

    Admin notice

--- theme/inc/customizer/fse-opt-in.php
+++ theme/inc/customizer/fse-opt-in.php
@@ -38,6 +38,16 @@ function setup() {
 
 	add_action( 'customize_register', __NAMESPACE__ . '\\register' );
 
+	if ( is_admin() && ! get_theme_mod( 'fse_opt_option', '' ) ) {
+		$notice_option = get_option( 'material_design_google', [] );
+		if ( ! isset( $notice_option['fse_opt_notice'] ) ) {
+			// Show notice to opt in.
+			add_action( 'admin_notices', __NAMESPACE__ . '\\admin_notice' );
+			add_action( 'wp_ajax_material_fse_notice', __NAMESPACE__ . '\\admin_dismiss_notice' );
+			add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\admin_enqueue_scripts' );
+		}
+	}
+
 	handle_fse_opt_out();
 }
 
@@ -130,8 +140,8 @@ function get_controls() {
 	return [
 		[
 			'id'          => 'fse_opt_option',
-			'label'       => esc_html__( 'Opt for Full site editing', 'material-design-google' ),
-			'description' => sprintf( '<p class="description">%s</p>', esc_html__( 'Full site editing feature is in beta. You can opt in or out for full site editing. Please test it out in local or staging environment before turning on in production.', 'material-design-google' ) ),
+			'label'       => esc_html__( 'Enable Full Site editing', 'material-design-google' ),
+			'description' => sprintf( '<p class="description">%s</p>', esc_html__( 'Full site editing is currently in beta. If you wish to enable the full site editing version of the theme, please test it out on a test environment before considering going live.', 'material-design-google' ) ),
 			'type'        => 'radio',
 			'default'     => 'out',
 			'choices'     => [
@@ -174,4 +184,103 @@ function filter_disable_fse( $path, $file ) {
  */
 function determine_template( $_template, $_type, $templates ) {
 	return locate_template( $templates );
+}
+
+/**
+ * Add admin notice to opt in or out.
+ *
+ * @return void
+ */
+function admin_notice() {
+	$customizer_url = admin_url( 'customize.php?autofocus[panel]=' . Customizer\get_slug() );
+	$customizer_url = add_query_arg( 'autofocus[section]', Customizer\prepend_slug( 'fse_opt' ), $customizer_url );
+
+	printf(
+		'<div id="material-theme-opt-in" class="notice notice-success is-dismissible" style="border-left-color:var(--mdc-theme-primary, #00a32a); transition:opacity 1s; opacity: 1;"><p>%s</p>',
+		esc_html__( 'Google Material Theme full site editing support is available. Please note this WordPress feature is currently in beta. If you wish to enable the full site editing version of the theme, please test it out on a test environment before going live.', 'material-design-google' )
+	);
+
+	printf( '<p class="hidden">%s</p>', esc_html__( 'You can enable this feature from customizer.', 'material-design-google' ) );
+
+	wp_nonce_field( 'fse_opt_notice', 'fse_opt_nonce' );
+
+	echo '<div class="button-group" style="padding-bottom: 5px;">';
+	printf( '<a href="%s"><button class="button button-primary" style="background-color: var(--mdc-theme-primary); color: var(--mdc-theme-on-primary); margin-right: 5px;">%s</button></a> ', esc_url( $customizer_url ), esc_html__( 'Preview in customizer', 'material-design-google' ) );
+	printf( '<button class="button button-secondary" style="/* background-color: var(--mdc-theme-secondary); color: var(--mdc-theme-on-secondary) */">%s</button>', esc_html__( 'Maybe later', 'material-design-google' ) );
+	echo '</div>';
+	?>
+	<script type="text/javascript">
+			( function() {
+					let callback = function() {
+						const notice = document.querySelector( "#material-theme-opt-in" );
+						var material_click_callback = function( e ) {
+							e.preventDefault();
+							const isAnchor = this.tagName === "A";
+							const buttonSecondary = this.classList.contains( "button-secondary" );
+							if ( buttonSecondary ) {
+								const p = document.querySelectorAll( "p" );
+								for ( let i = 0; i < p.length; i ++ ) {
+									p[i].classList.toggle( "hidden" );
+								}
+							}
+							const link = this.href;
+							wp.ajax.post( "material_fse_notice", {
+								_wpnonce: notice.querySelector( "input#fse_opt_nonce" ).value
+							} ).done( function( response ) {
+								if ( isAnchor ) {
+									location.href = link;
+								}
+								if ( buttonSecondary ) {
+									setTimeout( function() {
+										notice.style.opacity = 0;
+										setTimeout( function() {
+											notice.remove();
+										}, 1000 );
+									}, 3000 );
+								} else {
+									notice.remove();
+								}
+							} );
+						};
+						const targets = notice.querySelectorAll( ".button-secondary, a, .notice-dismiss" );
+						for ( var i = 0; i < targets.length; i ++ ) {
+							targets[i].addEventListener( "click", material_click_callback );
+						}
+					};
+					document.addEventListener( "DOMContentLoaded", () => setTimeout( callback, 1 ) );
+				}
+			)();
+	</script>
+
+	<?php
+
+	echo '</div>';
+}
+
+/**
+ * Save dismiss notice for opt in.
+ *
+ * @return void
+ */
+function admin_dismiss_notice() {
+	if ( ! isset( $_POST['_wpnonce'] ) || ! wp_verify_nonce( $_POST['_wpnonce'], 'fse_opt_notice' ) ) { // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+		return;
+	}
+
+	$value = get_option( 'material_design_google', [] );
+
+	$value['fse_opt_notice'] = true;
+
+	update_option( 'material_design_google', $value );
+
+	wp_send_json_success();
+}
+
+/**
+ * Admin enqueue scripts dependency.
+ *
+ * @return void
+ */
+function admin_enqueue_scripts() {
+	wp_enqueue_script( 'wp-util' );
 }
