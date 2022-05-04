23fd1dd0339672bd2b3be117b300037892315153
Utkarsh Patel <itismeutkarsh@gmail.com>
2022-05-04 17:58:52 +0530

    Add masonry for query loop

--- plugin/assets/src/block-editor/style/core-template/index.js
+++ plugin/assets/src/block-editor/style/core-template/index.js
@@ -14,29 +14,9 @@
  * limitations under the License.
  */
 
-/* istanbul ignore file */
+import { registerBlockStyle } from '@wordpress/blocks';
 
-/**
- * WordPress dependencies
- */
-import { updateCategory } from '@wordpress/blocks';
-
-/**
- * Internal dependencies
- */
-import { registerBlocks, MaterialLogo } from './helpers';
-import './blocks/data-table/hooks';
-import './formats';
-import './style/core-template';
-
-/**
- * Register the blocks.
- */
-registerBlocks( require.context( './blocks', true, /(?<!test\/)index\.js$/ ) );
-
-/**
- * Update the material category icon to the material logo.
- */
-updateCategory( 'material', {
-	icon: () => <MaterialLogo />,
+registerBlockStyle( 'core/post-template', {
+	name: 'material-masonry',
+	label: 'Material Masonry Grid',
 } );
---
+++ plugin/assets/src/front-end/masonry.js
@@ -0,0 +1,104 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+let gridElement = null;
+let rowHeight = 24;
+let rowGap = 24;
+export const masonryInit = () => {
+	gridElement = document.querySelector( '.is-flex-container' );
+
+	if ( ! gridElement ) {
+		return;
+	}
+
+	const mediaQuery = window.matchMedia( '(min-width: 840px)' );
+
+	mediaQuery.addEventListener( 'change', handleResize );
+	handleResize( mediaQuery );
+};
+
+const handleResize = mediaQuery => {
+	if ( mediaQuery.matches ) {
+		resizeAllGridItems();
+	}
+};
+
+const resizeAllGridItems = () => {
+	const cells = gridElement.querySelectorAll(
+		'.is-style-material-masonry .wp-block-post'
+	);
+
+	if ( cells.length <= 0 ) {
+		return;
+	}
+
+	rowHeight = parseInt(
+		window
+			.getComputedStyle( gridElement )
+			.getPropertyValue( 'grid-auto-rows' ),
+		10
+	);
+
+	rowGap = parseInt(
+		window
+			.getComputedStyle( gridElement )
+			.getPropertyValue( 'grid-row-gap' ),
+		10
+	);
+
+	const hasPostCard = cells[ 0 ].querySelectorAll( '.post-card' ).length > 0;
+
+	if ( ! hasPostCard ) {
+		gridElement.style.gridAutoRows = 'minmax(min-content,max-content)';
+
+		// Let it re-render to compute height.
+		setTimeout( () => {
+			cells.forEach( resizeGridItem );
+			gridElement.style.removeProperty( 'grid-auto-rows' );
+		}, 0 );
+		return;
+	}
+
+	cells.forEach( resizeGridItem );
+};
+
+const resizeGridItem = cell => {
+	if ( ! cell ) {
+		return;
+	}
+
+	let cellCard = cell.querySelector( '.post-card' );
+
+	if ( ! cellCard ) {
+		const imageCardBlock = cell.querySelector(
+			'.wp-block-material-image-card-query'
+		);
+		if ( imageCardBlock ) {
+			// For material image card.
+			cellCard = imageCardBlock;
+		} else {
+			// If we have a cell without card wrapper inside let's use that, Used for default WP template.
+			cellCard = cell;
+		}
+	}
+
+	const contentHeight = cellCard.getBoundingClientRect().height;
+
+	const rowSpan = Math.ceil(
+		( contentHeight + rowGap ) / ( rowHeight + rowGap )
+	);
+
+	cell.style.gridRowEnd = 'span ' + rowSpan;
+};
