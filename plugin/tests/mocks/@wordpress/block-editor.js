45ee6cf63762f444b5588af2fb9fa1563ab91266
Mario Aguiar <mario.aguiar@xwp.co>
2022-08-11 10:24:26 -0500

    Mock useSetting

--- plugin/tests/mocks/@wordpress/block-editor.js
+++ plugin/tests/mocks/@wordpress/block-editor.js
@@ -108,4 +108,147 @@ module.exports = {
 			</div>
 		</div>
 	),
+	useSetting: setting => {
+		switch ( setting ) {
+			case 'color.palette':
+				return [
+					{
+						slug: 'primary',
+						name: 'Primary',
+						color: 'var(--md-sys-color-primary)',
+					},
+					{
+						slug: 'on-primary',
+						name: 'On Primary',
+						color: 'var(--md-sys-color-on-primary)',
+					},
+					{
+						slug: 'primary-container',
+						name: 'Primary Container',
+						color: 'var(--md-sys-color-primary-container)',
+					},
+					{
+						slug: 'on-primary-container',
+						name: 'On Primary Container',
+						color: 'var(--md-sys-color-on-primary-container)',
+					},
+					{
+						slug: 'secondary',
+						name: 'Secondary',
+						color: 'var(--md-sys-color-secondary)',
+					},
+					{
+						slug: 'on-secondary',
+						name: 'On Secondary',
+						color: 'var(--md-sys-color-on-secondary)',
+					},
+					{
+						slug: 'secondary-container',
+						name: 'Secondary Container',
+						color: 'var(--md-sys-color-secondary-container)',
+					},
+					{
+						slug: 'on-secondary-container',
+						name: 'On Secondary Container',
+						color: 'var(--md-sys-color-on-secondary-container)',
+					},
+					{
+						slug: 'tertiary',
+						name: 'Tertiary',
+						color: 'var(--md-sys-color-tertiary)',
+					},
+					{
+						slug: 'on-tertiary',
+						name: 'On Tertiary',
+						color: 'var(--md-sys-color-on-tertiary)',
+					},
+					{
+						slug: 'tertiary-container',
+						name: 'Tertiary Container',
+						color: 'var(--md-sys-color-tertiary-container)',
+					},
+					{
+						slug: 'on-tertiary-container',
+						name: 'On Tertiary Container',
+						color: 'var(--md-sys-color-on-tertiary-container)',
+					},
+					{
+						slug: 'error',
+						name: 'Error',
+						color: 'var(--md-sys-color-error)',
+					},
+					{
+						slug: 'on-error',
+						name: 'On Error',
+						color: 'var(--md-sys-color-on-error)',
+					},
+					{
+						slug: 'error-container',
+						name: 'Error Container',
+						color: 'var(--md-sys-color-error-container)',
+					},
+					{
+						slug: 'on-error-container',
+						name: 'On Error Container',
+						color: 'var(--md-sys-color-on-error-container)',
+					},
+					{
+						slug: 'background',
+						name: 'Background',
+						color: 'var(--md-sys-color-background)',
+					},
+					{
+						slug: 'on-background',
+						name: 'On Background',
+						color: 'var(--md-sys-color-on-background)',
+					},
+					{
+						slug: 'surface',
+						name: 'Surface',
+						color: 'var(--md-sys-color-surface)',
+					},
+					{
+						slug: 'on-surface',
+						name: 'On Surface',
+						color: 'var(--md-sys-color-on-surface)',
+					},
+					{
+						slug: 'surface-variant',
+						name: 'Surface Variant',
+						color: 'var(--md-sys-color-surface-variant)',
+					},
+					{
+						slug: 'on-surface-variant',
+						name: 'On Surface Variant',
+						color: 'var(--md-sys-color-on-surface-variant)',
+					},
+					{
+						slug: 'outline',
+						name: 'Outline',
+						color: 'var(--md-sys-color-outline)',
+					},
+					{
+						slug: 'shadow',
+						name: 'Shadow',
+						color: 'var(--md-sys-color-shadow)',
+					},
+					{
+						slug: 'inverse-surface',
+						name: 'Surface',
+						color: 'var(--md-sys-color-inverse-surface)',
+					},
+					{
+						slug: 'inverse-on-surface',
+						name: 'Inverse On Surface',
+						color: 'var(--md-sys-color-inverse-on-surface)',
+					},
+					{
+						slug: 'inverse-primary',
+						name: 'Inverse Primary',
+						color: 'var(--md-sys-color-inverse-primary)',
+					},
+				];
+				break;
+		}
+	},
 };
