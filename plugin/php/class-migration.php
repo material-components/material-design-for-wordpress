e59bdc97e1358a61c48dc9174e01b3346d773c01
Utkarsh Patel <itismeutkarsh@gmail.com>
2022-08-17 16:14:52 +0530

    Add migration from m2 to m3 for color; Add admin notice

--- plugin/php/class-migration.php
+++ plugin/php/class-migration.php
@@ -25,19 +25,39 @@
 
 namespace MaterialDesign\Plugin;
 
+use function MaterialDesign\Theme\Customizer\get_slug;
+
 /**
- * Migration class.
+ * Migration class from m2 to m3.
  */
 class Migration extends Module_Base {
 
 	const MIGRATION_KEY = 'material_m2_to_m3_migration';
 
+	private $slug = '';
+
 	/**
 	 * Initiate the class and hooks.
 	 */
 	public function init() {
-		add_action( 'admin_init', [ $this, 'migrate_m2_typography_settings_to_m3' ], 10 );
-		add_action( 'admin_init', [ $this, 'migrate_m2_corner_radius' ], 10 );
+		$this->slug = $this->plugin->customizer_controls->slug;
+
+		add_action( 'admin_init', [ $this, 'migrate_m2_typography_settings_to_m3' ] );
+		add_action( 'admin_init', [ $this, 'migrate_m2_corner_radius' ] );
+		add_action( 'admin_init', [ $this, 'migrate_colors' ] );
+		add_action( 'admin_notices', [ $this, 'admin_notice_m3_migration' ] );
+
+		add_action( 'wp_ajax_m3_migrate_colors', [ $this, 'ajax_migrate_colors' ] );
+		add_action( 'wp_ajax_material_m3_notice', [ $this, 'notice_click' ] );
+	}
+
+	/**
+	 * Get customizer option.
+	 *
+	 * @return array
+	 */
+	public function get_customizer_option() {
+		return get_option( $this->slug, [] );
 	}
 
 	/**
@@ -95,9 +115,7 @@ class Migration extends Module_Base {
 			'head_font_family' => [ 'display_font_family', 'headline_font_family' ],
 		];
 
-		$slug = $this->plugin->customizer_controls->slug;
-
-		$option = get_option( $slug, [] );
+		$option = $this->get_customizer_option();
 
 		foreach ( $map as $m2_key => $m3_key ) {
 			if ( is_array( $m3_key ) ) {
@@ -115,7 +133,7 @@ class Migration extends Module_Base {
 			}
 		}
 
-		update_option( $slug, $option );
+		update_option( $this->slug, $option );
 
 		$this->update( 'typography' );
 	}
@@ -143,6 +161,146 @@ class Migration extends Module_Base {
 		$option = array_merge( $option, $radius );
 		update_option( $slug, $option );
 		$this->update( 'corner_radius' );
+	}
+
+	/**
+	 * Migrate colors.
+	 *
+	 * @return void
+	 */
+	public function migrate_colors() {
+		if ( $this->get( 'color_migration' ) ) {
+			return;
+		}
+		add_action( 'admin_enqueue_scripts', [ $this, 'admin_enqueue_scripts' ] );
+	}
+
+	/**
+	 * Admin enqueue script for color migration.
+	 *
+	 * @return void
+	 */
+	public function admin_enqueue_scripts() {
+		$option = $this->get_customizer_option();
+
+		if ( isset( $option['color_palette'] ) ) {
+			return;
+		}
+
+		$version = include $this->plugin->dir_path . '/assets/js/admin-m3-color-migration.asset.php';
+
+		wp_enqueue_script(
+			'material-m3-migration-color',
+			$this->plugin->asset_url( 'assets/js/admin-m3-color-migration.js' ),
+			$version['dependencies'],
+			$version['version'],
+			true
+		);
+
+		wp_localize_script(
+			'material-m3-migration-color',
+			'material_m3_migration_color',
+			[
+				'nonce'        => wp_create_nonce( 'material_m3_migration_color' ),
+				'primaryColor' => $option['primary_color'],
+			]
+		);
+	}
+
+	/**
+	 * Ajax migrate colors.
+	 *
+	 * @return void
+	 */
+	public function ajax_migrate_colors() {
+		if ( ! isset( $_POST['_wpnonce'] ) || ! wp_verify_nonce( $_POST['_wpnonce'], 'material_m3_migration_color' ) ) { // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+			return;
+		}
+
+		$option = $this->get_customizer_option();
+		if ( isset( $option['color_palette'] ) ) {
+			// We already got the palette, abort.
+			wp_send_json_success();
+		}
+
+		$color_palette = json_decode( wp_unslash( $_POST['colorPalette'] ), true ); // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+
+		$sanitized_color = [
+			'source'   => intval( $color_palette['source'] ),
+			'schemes'  => [],
+			'palettes' => [],
+		];
+
+		foreach ( $color_palette['schemes'] as $key => $values ) {
+			// Sanitize value by passing as ref and changing the value.
+			array_walk( $values, function ( &$val ) {
+				$val = intval( $val );
+			} );
+			$sanitized_color['schemes'][ $key ] = $values;
+		}
+
+		foreach ( $color_palette['palettes'] as $key => $palette ) {
+			$sanitized_color['palettes'][ $key ] = [
+				'primary' => floatval( $palette['hue'] ),
+				'chroma'  => intval( $palette['chroma'] ),
+				'cache'   => []
+			];
+		}
+
+		$option['color_palette'] = json_encode( $sanitized_color );
+		update_option( $this->slug, $option );
+		$this->update( 'color_migration' );
+		wp_send_json_success();
+	}
+
+	/**
+	 * Show admin notice when migrated from m2 to m3.
+	 *
+	 * @return void
+	 */
+	public function admin_notice_m3_migration() {
+		if ( $this->get( 'admin_notice' ) ) {
+			return;
+		}
+		$version = include $this->plugin->dir_path . '/assets/js/admin-notice-m3-migration.asset.php';
+
+		wp_enqueue_script(
+			'material-m3-notice-migration-color',
+			$this->plugin->asset_url( 'assets/js/admin-notice-m3-migration.js' ),
+			$version['dependencies'],
+			$version['version'],
+			true
+		);
+
+		wp_localize_script(
+			'material-m3-notice-migration-color',
+			'material_m3_migration_notice',
+			[
+				'nonce' => wp_create_nonce( 'material_m3_migration_notice' ),
+			]
+		);
+
+		$customizer_url = admin_url( 'customize.php?autofocus[panel]=' . get_slug() );
+		printf(
+			'<div id="material-theme-m3-migration" class="notice notice-info is-dismissible" style="transition:opacity 1s; opacity: 1;"><p>%s</p>',
+			esc_html__( 'Your site has been updated with Material Design 3. The color palette will be generated dynamically from primary color. You can preview the site and change the source color from customizer.', 'material-design-google' )
+		);
+		echo '<div class="button-group" style="padding-bottom: 5px;">';
+		printf( '<a class="material-theme-m3-migration__primary" href="%s"><button class="button button-primary" style="margin-right: 7px;">%s</button></a> ', esc_url( $customizer_url ), esc_html__( 'Go to customizer', 'material-design-google' ) );
+		echo '</div></div>';
+	}
+
+	/**
+	 * Handle notice click.
+	 *
+	 * @return void
+	 */
+	public function notice_click() {
+		if ( ! isset( $_POST['_wpnonce'] ) || ! wp_verify_nonce( $_POST['_wpnonce'], 'material_m3_migration_notice' ) ) { // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+			return;
+		}
+		$this->update( 'admin_notice' );
+		wp_send_json_success();
 	}
 
 }
